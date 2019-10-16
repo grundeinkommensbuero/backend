4485c33abf9d74c40d101f5e7395c4bfda3c8c4c
Valioesi <Vali_Schagerl@web.de>
2019-10-16 16:03:22 +0200

    added basic custom email

--- lambda-api/createUserinDynamo/index.js
+++ lambda-api/createUserinDynamo/index.js
@@ -3,40 +3,58 @@ const AWS = require("aws-sdk");
 const ddb = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async event => {
-  const tableName = process.env.TABLE_NAME;
+  // Identify why was this function invoked
+  if (event.triggerSource === "CustomMessage_SignUp") {
+    const tableName = process.env.TABLE_NAME;
 
-  //sub is the unique id Cognito assigns to each new user
-  const cognitoId = event.request.userAttributes.sub;
+    //sub is the unique id Cognito assigns to each new user
+    const cognitoId = event.request.userAttributes.sub;
 
-  const email = event.request.userAttributes.email;
+    const email = event.request.userAttributes.email;
 
-  const date = new Date();
-  const timestamp = date.toISOString();
+    const date = new Date();
+    const timestamp = date.toISOString();
 
-  //check, if parameters exist
-  if (cognitoId && email) {
-    //if it is the case proceed in saving the user in the dynamo db
+    //check, if parameters exist
+    if (cognitoId && email) {
+      //if it is the case proceed in saving the user in the dynamo db
 
-    try {
-      await ddb
-        .put({
-          TableName: tableName,
-          Item: {
-            cognitoId: cognitoId,
-            email: email,
-            createdAt: timestamp,
-            pledge: {}
-          }
-        })
-        .promise();
+      try {
+        await ddb
+          .put({
+            TableName: tableName,
+            Item: {
+              cognitoId: cognitoId,
+              email: email,
+              createdAt: timestamp,
+              pledge: {}
+            }
+          })
+          .promise();
 
-      console.log("Success writing to dynamo");
-      return event;
-    } catch (error) {
-      console.log("Error while writing to dynamo", error);
-      return event;
+        console.log("Success writing to dynamo");
+
+        //customize email
+        const codeParameter = event.request.codeParameter;
+        console.log("event response before", event.response);
+        event.response.emailSubject = "Dein Bestätigungslink";
+        event.response.emailMessage = customEmail(email, codeParameter);
+        console.log("event response after", event.response);
+        return event;
+      } catch (error) {
+        console.log("Error while writing to dynamo", error);
+        return event;
+      }
     }
+    console.log("One or more parameters missing");
+    return event;
   }
-  console.log("One or more parameters missing");
-  return event;
+};
+
+const customEmail = (email, codeParameter) => {
+  return `<p>Drück den Button<p>
+      <a href="https://dev.xbge.de/verification/?email=${email}&code=${codeParameter}">
+        <button>Verifizierung<button>
+      </a>
+  `;
 };
