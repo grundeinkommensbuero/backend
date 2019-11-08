ed92af5a411818bc71e9d4fa688a60f83f4ef362
Timur Celikel <me@timurcelikel.com>
2019-11-08 16:22:45 +0100

    createPdf: refactoring, cleanup

--- lambda-api/createPDF/index.js
+++ lambda-api/createPDF/index.js
@@ -7,57 +7,9 @@ const existingPdfBytes = fs.readFileSync('./test-list.pdf');
 const CODE = '0123456789';
 const URL = 'https://expedition-grundeinkommen.de/scan?id=';
 
-function getBarcode(text) {
-  return new Promise(resolve => {
-    bwipjs.toBuffer(
-      {
-        bcid: 'code128', // Barcode type
-        text: text, // Text to encode
-        scale: 3, // 3x scaling factor
-        height: 10, // Bar height, in millimeters
-        includetext: true, // Show human-readable text
-        textxalign: 'center', // Always good to set this
-        rotate: 'R',
-      },
-      function(err, png) {
-        if (err) {
-          // Decide how to handle the error
-          // `err` may be a string or Error object
-        } else {
-          resolve(png);
-        }
-      }
-    );
-  });
-}
-
-function getQrCode(text) {
-  return new Promise(resolve => {
-    bwipjs.toBuffer(
-      {
-        bcid: 'qrcode', // Barcode type
-        text: text, // Text to encode
-        scale: 3, // 3x scaling factor
-        height: 100, // Bar height, in millimeters
-        width: 100, // Bar height, in millimeters
-      },
-      function(err, png) {
-        if (err) {
-          // Decide how to handle the error
-          // `err` may be a string or Error object
-        } else {
-          resolve(png);
-        }
-      }
-    );
-  });
-}
-
-async function test() {
-  // Load a PDFDocument from the existing PDF bytes
+async function generatePdf() {
   const pdfDoc = await pdfLib.PDFDocument.load(existingPdfBytes);
 
-  // Get the first page of the document
   const pages = pdfDoc.getPages();
   const firstPage = pages[0];
 
@@ -79,10 +31,49 @@ async function test() {
     height: 90,
   });
 
-  // Serialize the PDFDocument to bytes (a Uint8Array)
   const pdfBytes = await pdfDoc.save();
 
   fs.writeFileSync('./test-list-out.pdf', pdfBytes);
 }
 
-test();
+function getBarcode(text) {
+  return new Promise(resolve => {
+    bwipjs.toBuffer(
+      {
+        bcid: 'code128',
+        text: text,
+        scale: 3,
+        height: 10,
+        includetext: true,
+        textxalign: 'center',
+        rotate: 'R',
+      },
+      function(err, png) {
+        if (!err) {
+          resolve(png);
+        }
+      }
+    );
+  });
+}
+
+function getQrCode(text) {
+  return new Promise(resolve => {
+    bwipjs.toBuffer(
+      {
+        bcid: 'qrcode',
+        text: text,
+        scale: 3,
+        height: 100,
+        width: 100,
+      },
+      function(err, png) {
+        if (!err) {
+          resolve(png);
+        }
+      }
+    );
+  });
+}
+
+generatePdf();
