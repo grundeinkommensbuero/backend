39050870847b9aa76e675dc502370b5430f47b9f
Valioesi <Vali_Schagerl@web.de>
2019-11-13 09:46:06 +0100

    upload generated pdf to s3

--- lambda-api/createPDF/index.js
+++ lambda-api/createPDF/index.js
@@ -1,5 +1,7 @@
 const generatePdf = require('./createPDF');
 const fs = require('fs');
+const AWS = require('aws-sdk');
+const S3 = new AWS.S3();
 
 const inputPDF = fs.readFileSync(__dirname + '/test-list.pdf');
 const URL = 'https://expedition-grundeinkommen.de/scan?id=';
@@ -9,13 +11,37 @@ exports.handler = async event => {
   const pdfId = event.pathParameters.pdfId;
   const generatedPdf = await generatePdf(URL, pdfId, inputPDF);
 
+  //upload file to S3
+  try {
+    const result = await S3.upload({
+      Bucket: 'signature-lists',
+      Key: `${pdfId}.pdf`,
+      Body: Buffer.from(generatedPdf),
+      ContentType: 'application/pdf',
+    }).promise();
+    console.log('success uploading pdf to bucket', result);
+    return response(201, 'Created PDF and uploaded it to bucket', {
+      id: pdfId,
+      url: result.Location,
+    });
+  } catch (error) {
+    console.log('error uploading pdf to bucket', error);
+    return response(500, error);
+  }
+};
+
+const response = (statusCode, message, signatureList = null) => {
+  const data = { message: message };
+  if (signatureList !== null) {
+    data.signatureList = signatureList;
+  }
   return {
-    statusCode: 200,
-    body: generatedPdf.toString('base64'),
-    isBase64Encoded: true,
+    statusCode: statusCode,
+    body: JSON.stringify(data),
+    isBase64Encoded: false,
     headers: {
       'Access-Control-Allow-Origin': '*',
-      'Content-type': 'application/pdf',
+      'Content-type': 'application/json',
     },
   };
 };
