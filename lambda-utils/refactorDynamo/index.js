4c308bc33104c0f1058f02a3b94a16fa2eb14a5d
Valioesi <Vali_Schagerl@web.de>
2019-11-18 11:02:41 +0100

    updated refactorDynamo

--- lambda-utils/refactorDynamo/index.js
+++ lambda-utils/refactorDynamo/index.js
@@ -1,7 +1,8 @@
 /**
  * This lambda is used to update the dynamo db by:
- * - renaming pledge key to something more specific (schleswig-holstein-1)
+ * - refactor pledge to be inside of an array
  * - add timestamp to newsletter consent
+ * (- renaming pledge key to something more specific (schleswig-holstein-1))
  */
 
 const AWS = require('aws-sdk');
@@ -14,15 +15,20 @@ exports.handler = async event => {
     for (let user of users.Items) {
       console.log('user', user);
       // check if user contains a valid pledge for the key 'pledge'
-      if ('pledge' in user && typeof user.pledge === 'object') {
+      if (
+        'pledge-schleswig-holstein-1' in user &&
+        typeof user['pledge-schleswig-holstein-1'] === 'object'
+      ) {
         //update name of pledge key/column
         try {
-          const result = await changePledgeKey(user);
+          const result = await movePledgeToArray(user);
           console.log('success changing pledge key', result);
         } catch (error) {
           console.log('error changing pledge key', error);
           break; //if there is something wrong, we want to break out of the loop
         }
+      } else {
+        console.log('not in user');
       }
       //refactor newsletter consent to use format {value: boolean, timestamp: string} instead of boolean
       //check if it is still in old format
@@ -46,7 +52,6 @@ exports.handler = async event => {
 const getAllUsers = () => {
   const params = {
     TableName: tableName,
-    ProjectionExpression: 'cognitoId, pledge, newsletterConsent, createdAt',
   };
   return ddb.scan(params).promise();
 };
@@ -61,6 +66,29 @@ const changePledgeKey = user => {
       ExpressionAttributeValues: { ':pledge': user.pledge },
       ExpressionAttributeNames: {
         '#pledgeKey': 'pledge-schleswig-holstein-1', //to work properly we need to use a placeholder (https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Expressions.ExpressionAttributeNames.html#ExpressionAttributeNames)
+      },
+      ReturnValues: 'UPDATED_NEW',
+    })
+    .promise();
+};
+
+const movePledgeToArray = user => {
+  const newPledge = user['pledge-schleswig-holstein-1'];
+  newPledge.campaign = constructCampaignId('schleswig-holstein-1');
+  //needs to be array because append_list works with an array
+  const pledge = [newPledge];
+  return ddb
+    .update({
+      TableName: tableName,
+      Key: { cognitoId: user.cognitoId },
+      UpdateExpression:
+        'set pledges = list_append(if_not_exists(pledges, :emptyList), :pledge) REMOVE #oldPledgeKey',
+      ExpressionAttributeValues: {
+        ':pledge': pledge,
+        ':emptyList': [],
+      },
+      ExpressionAttributeNames: {
+        '#oldPledgeKey': 'pledge-schleswig-holstein-1', //to work properly we need to use a placeholder (https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Expressions.ExpressionAttributeNames.html#ExpressionAttributeNames)
       },
       ReturnValues: 'UPDATED_NEW',
     })
@@ -82,4 +110,18 @@ const changeNewsletterConsent = user => {
       ReturnValues: 'UPDATED_NEW',
     })
     .promise();
+};
+
+const constructCampaignId = campaignCode => {
+  const campaign = {};
+  if (typeof campaignCode !== 'undefined') {
+    //we want to remove the last characters from the string (brandenburg-2 -> brandenburg)
+    campaign.state = campaignCode.substring(0, campaignCode.length - 2);
+    //...and take the last char and save it as number
+    campaign.round = parseInt(
+      campaignCode.substring(campaignCode.length - 1, campaignCode.length)
+    );
+    campaign.code = campaignCode;
+  }
+  return campaign;
 };
