39c5160965a718847a48e58bd9d92b609e196b8b
Valioesi <Vali_Schagerl@web.de>
2019-11-28 14:12:02 +0100

    added trigger lambdas to serverless

---
+++ serverless/src/triggers/pinpointOptIn/index.js
@@ -0,0 +1,55 @@
+const AWS = require('aws-sdk');
+const pinpoint = new AWS.Pinpoint({ region: 'eu-central-1' });
+const projectId = '83c543b1094c4a91bf31731cd3f2f005';
+const ddb = new AWS.DynamoDB.DocumentClient();
+const tableName = require('./../../../config').usersTableName;
+
+module.exports.handler = async event => {
+  //extract the cognito id from the request (userName is userId)
+  console.log('event', event);
+  const userId = event.userName;
+  try {
+    const result = await getUser(userId);
+    console.log('result', result);
+    if ('Item' in result) {
+      //if the newsletter consent is true, we opt in in pinpoint
+      if (result.Item.newsletterConsent) {
+        try {
+          const updatedPinpoint = await updatePinpoint(result.Item);
+          console.log('success updating pinpoint', updatedPinpoint);
+        } catch (error) {
+          console.log('error updating pinpoint', error);
+        }
+      }
+    } else {
+      console.log('No user found');
+    }
+  } catch (error) {
+    console.log('error while getting user', error);
+  }
+  return event;
+};
+
+const getUser = userId => {
+  return ddb
+    .get({
+      TableName: tableName,
+      Key: {
+        cognitoId: userId,
+      },
+    })
+    .promise();
+};
+
+const updatePinpoint = user => {
+  const params = {
+    ApplicationId: projectId,
+    EndpointId: `email-endpoint-${user.cognitoId}`,
+    EndpointRequest: {
+      ChannelType: 'EMAIL',
+      Address: user.email,
+      OptOut: 'NONE',
+    },
+  };
+  return pinpoint.updateEndpoint(params).promise();
+};
---
+++ serverless/src/triggers/sendVerificationMail/index.js
@@ -0,0 +1,104 @@
+const AWS = require('aws-sdk');
+const fs = require('fs');
+const tableName = require('./../../../config').usersTableName;
+
+const htmlMail = fs.readFileSync('./mailTemplate.html', 'utf8');
+
+const ddb = new AWS.DynamoDB.DocumentClient();
+
+// this lambda not only sends the verification mail
+// but also creates a record for the user in dynamo
+module.exports.handler = async event => {
+  // Identify why was this function invoked
+  if (event.triggerSource === 'CustomMessage_SignUp') {
+    //sub is the unique id Cognito assigns to each new user
+    const cognitoId = event.request.userAttributes.sub;
+
+    const email = event.request.userAttributes.email;
+
+    const date = new Date();
+    const timestamp = date.toISOString();
+
+    //check, if parameters exist
+    if (cognitoId && email) {
+      //if it is the case proceed in saving the user in the dynamo db
+
+      try {
+        await ddb
+          .put({
+            TableName: tableName,
+            Item: {
+              cognitoId: cognitoId,
+              email: email,
+              createdAt: timestamp,
+            },
+          })
+          .promise();
+
+        console.log('Success writing to dynamo');
+
+        //customize email
+        const codeParameter = event.request.codeParameter;
+        event.response.emailSubject =
+          'Bitte bestätige deine E-Mail-Adresse für die Expedition Grundeinkommen!';
+        event.response.emailMessage = customEmail(email, codeParameter);
+        return event;
+      } catch (error) {
+        console.log('Error while writing to dynamo', error);
+        return event;
+      }
+    }
+    console.log('One or more parameters missing');
+    return event;
+  } else if (event.triggerSource === 'CustomMessage_ResendCode') {
+    const email = event.request.userAttributes.email;
+    //customize email
+    const codeParameter = event.request.codeParameter;
+    event.response.emailSubject =
+      'Volksabstimmung Grundeinkommensexperiment: Bitte bestätige deine E-Mail-Adresse!';
+    event.response.emailMessage = customReminderEmail(email, codeParameter);
+    console.log('Sending verification reminder');
+    return event;
+  }
+};
+
+const customEmail = (email, codeParameter) => {
+  const link = `https://expedition-grundeinkommen.de/verifizierung/?email=${email}&code=${codeParameter}`;
+  return htmlMail.replace(/\[\[VERIFICATION_LINK\]\]/gi, link);
+};
+
+const customReminderEmail = (email, codeParameter) => {
+  return `<p>Hallo,</p>
+          <p>
+          du hast deine E-Mail-Adresse f&#252;r die Expedition Grundeinkommen noch nicht best&#228;tigt.
+          Das ist besonders wichtig, da wir deine Daten ohne diese Best&#228;tigung leider wieder l&#246;schen m&#252;ssen.
+          Nur noch ein Klick auf diesen Link und du bist wirklich dabei:
+          </p>
+          <p>
+              <a href="https://expedition-grundeinkommen.de/verifizierung/?email=${email}&code=${codeParameter}">
+                 https://expedition-grundeinkommen.de/verifizierung/?email=${email}&code=${codeParameter}
+              </a>
+          </p>
+          <br>
+          <p>
+          Danke!
+          </p>
+          Dein Support-Team von<br>
+          Expedition Grundeinkommen
+          <br>
+          <br>
+          <br>
+          <br>
+          --------------------------------------------
+          <br>
+          <p>
+            <a href="www.expedition-grundeinkommen.de">
+              www.expedition-grundeinkommen.de
+            </a>
+            <br>
+            <a href="mailto:support@expedition-grundeinkommen.de">
+              support@expedition-grundeinkommen.de
+            </a>
+          </p>
+  `;
+};
