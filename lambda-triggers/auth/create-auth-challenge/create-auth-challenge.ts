b16201934f86827ad13eca7234c97fc999edc6f9
Valioesi <Vali_Schagerl@web.de>
2019-12-27 11:21:52 +0100

    germant text for secret login code

--- lambda-triggers/auth/create-auth-challenge/create-auth-challenge.ts
+++ lambda-triggers/auth/create-auth-challenge/create-auth-challenge.ts
@@ -8,60 +8,60 @@ import { SES } from 'aws-sdk';
 const ses = new SES();
 
 export const handler: CognitoUserPoolTriggerHandler = async event => {
+  let secretLoginCode: string;
+  if (!event.request.session || !event.request.session.length) {
+    // This is a new auth session
+    // Generate a new secret login code and mail it to the user
+    secretLoginCode = randomDigits(6).join('');
+    await sendEmail(event.request.userAttributes.email, secretLoginCode);
+  } else {
+    // There's an existing session. Don't generate new digits but
+    // re-use the code from the current session. This allows the user to
+    // make a mistake when keying in the code and to then retry, rather
+    // the needing to e-mail the user an all new code again.
+    const previousChallenge = event.request.session.slice(-1)[0];
+    secretLoginCode = previousChallenge.challengeMetadata!.match(
+      /CODE-(\d*)/
+    )![1];
+  }
 
-    let secretLoginCode: string;
-    if (!event.request.session || !event.request.session.length) {
+  // This is sent back to the client app
+  event.response.publicChallengeParameters = {
+    email: event.request.userAttributes.email,
+  };
 
-        // This is a new auth session
-        // Generate a new secret login code and mail it to the user
-        secretLoginCode = randomDigits(6).join('');
-        await sendEmail(event.request.userAttributes.email, secretLoginCode);
+  // Add the secret login code to the private challenge parameters
+  // so it can be verified by the "Verify Auth Challenge Response" trigger
+  event.response.privateChallengeParameters = { secretLoginCode };
 
-    } else {
+  // Add the secret login code to the session so it is available
+  // in a next invocation of the "Create Auth Challenge" trigger
+  event.response.challengeMetadata = `CODE-${secretLoginCode}`;
 
-        // There's an existing session. Don't generate new digits but
-        // re-use the code from the current session. This allows the user to
-        // make a mistake when keying in the code and to then retry, rather
-        // the needing to e-mail the user an all new code again.    
-        const previousChallenge = event.request.session.slice(-1)[0];
-        secretLoginCode = previousChallenge.challengeMetadata!.match(/CODE-(\d*)/)![1];
-    }
-
-    // This is sent back to the client app
-    event.response.publicChallengeParameters = { email: event.request.userAttributes.email };
-
-    // Add the secret login code to the private challenge parameters
-    // so it can be verified by the "Verify Auth Challenge Response" trigger
-    event.response.privateChallengeParameters = { secretLoginCode };
-
-    // Add the secret login code to the session so it is available
-    // in a next invocation of the "Create Auth Challenge" trigger
-    event.response.challengeMetadata = `CODE-${secretLoginCode}`;
-
-    return event;
+  return event;
 };
 
 async function sendEmail(emailAddress: string, secretLoginCode: string) {
-    const params: SES.SendEmailRequest = {
-        Destination: { ToAddresses: [emailAddress] },
-        Message: {
-            Body: {
-                Html: {
-                    Charset: 'UTF-8',
-                    Data: `<html><body><p>This is your secret login code:</p>
-                           <h3>${secretLoginCode}</h3></body></html>`
-                },
-                Text: {
-                    Charset: 'UTF-8',
-                    Data: `Your secret login code: ${secretLoginCode}`
-                }
-            },
-            Subject: {
-                Charset: 'UTF-8',
-                Data: 'Your secret login code'
-            }
+  const params: SES.SendEmailRequest = {
+    Destination: { ToAddresses: [emailAddress] },
+    Message: {
+      Body: {
+        Html: {
+          Charset: 'UTF-8',
+          Data: `<html><body><p>Hier ist dein geheimer Login-Code:</p>
+                           <h3>${secretLoginCode}</h3></body></html>`,
         },
-        Source: process.env.SES_FROM_ADDRESS!
-    };
-    await ses.sendEmail(params).promise();
+        Text: {
+          Charset: 'UTF-8',
+          Data: `Dein geheimer Login-Code: ${secretLoginCode}`,
+        },
+      },
+      Subject: {
+        Charset: 'UTF-8',
+        Data: `Dein geheimer Login-Code: ${secretLoginCode}`,
+      },
+    },
+    Source: process.env.SES_FROM_ADDRESS!,
+  };
+  await ses.sendEmail(params).promise();
 }
