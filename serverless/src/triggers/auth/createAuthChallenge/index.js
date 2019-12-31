6d375c87bba67161d19597963d67cba4469ced43
Valioesi <Vali_Schagerl@web.de>
2019-12-31 15:01:57 +0100

    transform typescript lambdas to js

--- serverless/src/triggers/auth/createAuthChallenge/index.js
+++ serverless/src/triggers/auth/createAuthChallenge/index.js
@@ -1,18 +1,12 @@
-// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: MIT-0
-
-import { CognitoUserPoolTriggerHandler } from 'aws-lambda';
-import { randomDigits } from 'crypto-secure-random-digit';
-import { SES } from 'aws-sdk';
-
-const ses = new SES();
-
-export const handler: CognitoUserPoolTriggerHandler = async event => {
-  let secretLoginCode: string;
+const crypto = require('crypto-secure-random-digit');
+const AWS = require('aws-sdk');
+const ses = new AWS.SES();
+exports.handler = async event => {
+  let secretLoginCode;
   if (!event.request.session || !event.request.session.length) {
     // This is a new auth session
     // Generate a new secret login code and mail it to the user
-    secretLoginCode = randomDigits(6).join('');
+    secretLoginCode = crypto.randomDigits(6).join('');
     await sendEmail(event.request.userAttributes.email, secretLoginCode);
   } else {
     // There's an existing session. Don't generate new digits but
@@ -20,29 +14,24 @@ export const handler: CognitoUserPoolTriggerHandler = async event => {
     // make a mistake when keying in the code and to then retry, rather
     // the needing to e-mail the user an all new code again.
     const previousChallenge = event.request.session.slice(-1)[0];
-    secretLoginCode = previousChallenge.challengeMetadata!.match(
+    secretLoginCode = previousChallenge.challengeMetadata.match(
       /CODE-(\d*)/
-    )![1];
+    )[1];
   }
-
   // This is sent back to the client app
   event.response.publicChallengeParameters = {
     email: event.request.userAttributes.email,
   };
-
   // Add the secret login code to the private challenge parameters
   // so it can be verified by the "Verify Auth Challenge Response" trigger
   event.response.privateChallengeParameters = { secretLoginCode };
-
   // Add the secret login code to the session so it is available
   // in a next invocation of the "Create Auth Challenge" trigger
   event.response.challengeMetadata = `CODE-${secretLoginCode}`;
-
   return event;
 };
-
-async function sendEmail(emailAddress: string, secretLoginCode: string) {
-  const params: SES.SendEmailRequest = {
+async function sendEmail(emailAddress, secretLoginCode) {
+  const params = {
     Destination: { ToAddresses: [emailAddress] },
     Message: {
       Body: {
@@ -61,7 +50,7 @@ async function sendEmail(emailAddress: string, secretLoginCode: string) {
         Data: `Dein geheimer Login-Code: ${secretLoginCode}`,
       },
     },
-    Source: process.env.fromAddress!,
+    Source: `Expedition Grundeinkommen <${process.env.SES_FROM_ADDRESS}>`,
   };
   await ses.sendEmail(params).promise();
 }
