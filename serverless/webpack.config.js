51e9d3601ad7fcd30d20c38eeb708b603963abff
Valioesi <Vali_Schagerl@web.de>
2020-01-02 12:39:58 +0100

    webpack: add htmls and pdfs per function

--- serverless/webpack.config.js
+++ serverless/webpack.config.js
@@ -5,11 +5,39 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 // (https://github.com/serverless-heaven/serverless-webpack/blob/master/examples/typescript/webpack.config.js)
 const isLocal = slsw.lib.webpack.isLocal;
 
+const functionsWithStaticFiles = [
+  'createSignatureList',
+  'adminCreateUser',
+  'sendCongratulationMails',
+  'sendVerificationMail',
+];
+
+// We need this to only copy html and pdf files for the corresponding function.
+// Based on https://github.com/serverless-heaven/serverless-webpack/issues/425
+// We only copy the files for each corresponding function
+const CopyStaticFilesPlugin = () => ({
+  apply: compiler => {
+    // Get the module name off of the output path
+    const moduleName = functionsWithStaticFiles.find(moduleName =>
+      compiler.options.output.path.includes(moduleName)
+    );
+
+    if (moduleName) {
+      console.log('module has static file dependencies:', moduleName);
+      new CopyWebpackPlugin([
+        `**/${moduleName}/mailTemplate.html`,
+        `**/${moduleName}/**/*.pdf`,
+      ]).apply(compiler);
+    } else {
+    }
+  },
+});
+
 module.exports = {
   mode: isLocal ? 'development' : 'production',
   // You can let the plugin determine the correct handler entry points at build time
   entry: slsw.lib.entries,
   target: 'node',
   stats: 'errors-warnings', // less logging
-  plugins: [new CopyWebpackPlugin(['**/*.html', '**/*.pdf'])],
+  plugins: [CopyStaticFilesPlugin()],
 };
