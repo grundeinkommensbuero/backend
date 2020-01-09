df1c7e0468e3fb169537e82201e5f842afc835d9
Timur Çelikel <me@timurcelikel.com>
2020-01-09 12:37:05 +0100

    createSignatureLists: refactor out PDFs and code locations

--- serverless/src/api/signatures/createSignatureList/createPDF.js
+++ serverless/src/api/signatures/createSignatureList/createPDF.js
@@ -1,71 +1,26 @@
-const fs = require('fs');
 const pdfLib = require('pdf-lib');
 const bwipjs = require('bwip-js');
+const inputPdfs = require('./inputPdfs');
 
-const inputPdfs = {
-  COMBINED: fs.readFileSync(__dirname + '/pdf/sh-1/ALLES_sw.pdf'),
-  SINGLE_SW: fs.readFileSync(__dirname + '/pdf/sh-1/1er_sw.pdf'),
-  MULTI_SW: fs.readFileSync(__dirname + '/pdf/sh-1/5er_sw.pdf'),
-  SINGLE: fs.readFileSync(__dirname + '/pdf/sh-1/1er.pdf'),
-  MULTI: fs.readFileSync(__dirname + '/pdf/sh-1/5er.pdf'),
-};
-
-const CODE_POSITIONS = {
-  BARCODE_SINGLE: {
-    x: 752,
-    y: 28,
-    width: 85,
-    height: 37,
-  },
-  QRCODE_SINGLE: {
-    x: 687,
-    y: 449,
-    width: 39,
-    height: 39,
-  },
-  BARCODE_MULTI: {
-    x: 535,
-    y: 20,
-    width: 90,
-    height: 40,
-  },
-  QRCODE_MULTI: {
-    x: 685,
-    y: 464,
-    width: 39,
-    height: 39,
-  },
-};
-
-module.exports = async function generatePdf(url, code, type) {
+module.exports = async function generatePdf(url, code, type, campaignCode) {
   console.log('generating pdf...');
-  const pdfDoc = await pdfLib.PDFDocument.load(inputPdfs[type]);
+  const pdfDoc = await pdfLib.PDFDocument.load(
+    inputPdfs[campaignCode][type].file
+  );
 
   const pages = pdfDoc.getPages();
 
   const barcode = await getBarcode(code);
-
   const barcodeInDocument = await pdfDoc.embedPng(barcode);
 
   const qrCode = await getQrCode(url + code);
   const qrCodeInDocument = await pdfDoc.embedPng(qrCode);
 
-  if (type === 'COMBINED') {
-    pages[1].drawImage(barcodeInDocument, CODE_POSITIONS.BARCODE_SINGLE);
-    pages[1].drawImage(qrCodeInDocument, CODE_POSITIONS.QRCODE_SINGLE);
-    pages[2].drawImage(barcodeInDocument, CODE_POSITIONS.BARCODE_MULTI);
-    pages[2].drawImage(qrCodeInDocument, CODE_POSITIONS.QRCODE_MULTI);
-  }
-
-  if (type === 'SINGLE' || type === 'SINGLE_SW') {
-    pages[0].drawImage(barcodeInDocument, CODE_POSITIONS.BARCODE_SINGLE);
-    pages[0].drawImage(qrCodeInDocument, CODE_POSITIONS.QRCODE_SINGLE);
-  }
-
-  if (type === 'MULTI' || type === 'MULTI_SW') {
-    pages[0].drawImage(barcodeInDocument, CODE_POSITIONS.BARCODE_MULTI);
-    pages[0].drawImage(qrCodeInDocument, CODE_POSITIONS.QRCODE_MULTI);
-  }
+  inputPdfs[campaignCode][type].codes.forEach(codeInfo => {
+    const codeImage =
+      codeInfo.type === 'BAR' ? barcodeInDocument : qrCodeInDocument;
+    pages[codeInfo.page].drawImage(codeImage, codeInfo.position);
+  });
 
   const pdfBytes = await pdfDoc.save();
   return pdfBytes;
---
+++ serverless/src/api/signatures/createSignatureList/inputPdfs.js
@@ -0,0 +1,118 @@
+const fs = require('fs');
+
+const CODE_POSITIONS = {
+  BARCODE_SINGLE: {
+    x: 752,
+    y: 28,
+    width: 85,
+    height: 37,
+  },
+  QRCODE_SINGLE: {
+    x: 687,
+    y: 449,
+    width: 39,
+    height: 39,
+  },
+  BARCODE_MULTI: {
+    x: 535,
+    y: 20,
+    width: 90,
+    height: 40,
+  },
+  QRCODE_MULTI: {
+    x: 685,
+    y: 464,
+    width: 39,
+    height: 39,
+  },
+};
+
+module.exports = {
+  'schleswig-holstein-1': {
+    COMBINED: {
+      file: fs.readFileSync(__dirname + '/pdf/sh-1/ALLES_sw.pdf'),
+      codes: [
+        {
+          type: 'BAR',
+          page: 1,
+          position: CODE_POSITIONS.BARCODE_SINGLE,
+        },
+        {
+          type: 'QR',
+          page: 1,
+          position: CODE_POSITIONS.QRCODE_SINGLE,
+        },
+        {
+          type: 'BAR',
+          page: 2,
+          position: CODE_POSITIONS.BARCODE_MULTI,
+        },
+        {
+          type: 'QR',
+          page: 2,
+          position: CODE_POSITIONS.QRCODE_MULTI,
+        },
+      ],
+    },
+    SINGLE_SW: {
+      file: fs.readFileSync(__dirname + '/pdf/sh-1/1er_sw.pdf'),
+      codes: [
+        {
+          type: 'BAR',
+          page: 0,
+          position: CODE_POSITIONS.BARCODE_SINGLE,
+        },
+        {
+          type: 'QR',
+          page: 0,
+          position: CODE_POSITIONS.QRCODE_SINGLE,
+        },
+      ],
+    },
+    SINGLE: {
+      file: fs.readFileSync(__dirname + '/pdf/sh-1/1er.pdf'),
+      codes: [
+        {
+          type: 'BAR',
+          page: 0,
+          position: CODE_POSITIONS.BARCODE_SINGLE,
+        },
+        {
+          type: 'QR',
+          page: 0,
+          position: CODE_POSITIONS.QRCODE_SINGLE,
+        },
+      ],
+    },
+    MULTI_SW: {
+      file: fs.readFileSync(__dirname + '/pdf/sh-1/5er_sw.pdf'),
+      codes: [
+        {
+          type: 'BAR',
+          page: 0,
+          position: CODE_POSITIONS.BARCODE_MULTI,
+        },
+        {
+          type: 'QR',
+          page: 0,
+          position: CODE_POSITIONS.QRCODE_MULTI,
+        },
+      ],
+    },
+    MULTI: {
+      file: fs.readFileSync(__dirname + '/pdf/sh-1/5er.pdf'),
+      codes: [
+        {
+          type: 'BAR',
+          page: 0,
+          position: CODE_POSITIONS.BARCODE_MULTI,
+        },
+        {
+          type: 'QR',
+          page: 0,
+          position: CODE_POSITIONS.QRCODE_MULTI,
+        },
+      ],
+    },
+  },
+};
