a189751887b3c167d2994bda39ba39dfb5a64b0b
Valioesi <Vali_Schagerl@web.de>
2020-01-13 12:15:02 +0100

    made fillPinpoint trigger lambda ready

---
+++ serverless/src/triggers/fillPinpoint/zipCodeMatcher/index.js
@@ -0,0 +1,20 @@
+const data = require('./data').data;
+
+const getStateByZipCode = zipCode => {
+  const item = data.find(entry => zipCode.startsWith(entry.plz));
+  if (typeof item !== 'undefined') {
+    return item.bundesland;
+  } else {
+    return 'undefined';
+  }
+};
+
+const getZipCodeByCity = city => {
+  const item = data.find(entry => city === entry.ort);
+  if (typeof item !== 'undefined') {
+    return item.plz.toString();
+  } else {
+    return undefined;
+  }
+};
+module.exports = { getStateByZipCode, getZipCodeByCity };
