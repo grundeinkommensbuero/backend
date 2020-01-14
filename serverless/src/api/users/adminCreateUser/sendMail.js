df6c26c1abc789999a95e20b6f673a429c2eb280
Timur Çelikel <me@timurcelikel.com>
2020-01-14 16:57:41 +0100

    adminCreateUser: wording for Brandenburg

--- serverless/src/api/users/adminCreateUser/sendMail.js
+++ serverless/src/api/users/adminCreateUser/sendMail.js
@@ -5,14 +5,26 @@ const fs = require('fs');
 
 const htmlMail = fs.readFileSync(__dirname + '/mailTemplate.html', 'utf8');
 
+const CAMPAIGN_SLUGS = {
+  'schleswig-holstein-1': 'schleswig-holstein',
+  'brandenburg-1': 'brandenburg',
+};
+
+const STATES = {
+  'schleswig-holstein-1': 'Schleswig-Holstein',
+  'brandenburg-1': 'Brandenburg',
+};
+
 //Function which sends an email to welcome the user to our expedition
 const sendMail = (email, campaignCode, userId) => {
   const mailOptions = {
     from: 'Expedition Grundeinkommen <support@expedition-grundeinkommen.de',
     subject: 'Danke für deine Unterschrift!',
     html: htmlMail
-      .replace('[[CAMPAIGN_CODE]]', campaignCode)
-      .replace('[[USER_ID]]', userId),
+      .replace(/\[\[CAMPAIGN_CODE\]\]/gi, campaignCode)
+      .replace(/\[\[CAMPAIGN_SLUG\]\]/gi, CAMPAIGN_SLUGS[campaignCode])
+      .replace(/\[\[STATE\]\]/gi, STATES[campaignCode])
+      .replace(/\[\[USER_ID\]\]/gi, userId),
     to: email,
   };
 
