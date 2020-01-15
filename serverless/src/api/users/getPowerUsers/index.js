449746db42a03bb29fc2934bf47aacf10cb37bca
Valioesi <Vali_Schagerl@web.de>
2020-01-15 15:31:04 +0100

    handle campaigns better, getting powerusers

--- serverless/src/api/users/getPowerUsers/index.js
+++ serverless/src/api/users/getPowerUsers/index.js
@@ -27,41 +27,47 @@ module.exports.handler = async event => {
       const signatureLists = await getSignatureListsOfUser(user.cognitoId);
 
       // For each list sum up the received and scanned by user signatures
-      let received = 0;
-      let scannedByUser = 0;
+      // We want to have object (e.g. { campaign1: { received, scannedByUser }, campaign2: ...})
+      // because someone might have signatures for multiple campaigns
+      let signatureCount = {};
 
       user.campaigns = [];
 
       for (let list of signatureLists) {
-        if ('received' in list) {
-          for (let scan of list.received) {
-            received += parseInt(scan.count);
+        if ('received' in list || 'scannedByUser' in list) {
+          // Initialize object
+          if (!(list.campaign.code in signatureCount)) {
+            signatureCount[list.campaign.code] = {
+              received: 0,
+              scannedByUser: 0,
+            };
           }
-        }
 
-        if ('scannedByUser' in list) {
-          for (let scan of list.scannedByUser) {
-            scannedByUser += parseInt(scan.count);
+          if ('received' in list) {
+            for (let scan of list.received) {
+              signatureCount[list.campaign.code].received += parseInt(
+                scan.count
+              );
+            }
           }
-        }
 
-        // Add the campaign (might also be multiple) to the user
-        if (!user.campaigns.includes(list.campaign.code)) {
-          user.campaigns.push(list.campaign.code);
+          if ('scannedByUser' in list) {
+            for (let scan of list.scannedByUser) {
+              signatureCount[list.campaign.code].scannedByUser += parseInt(
+                scan.count
+              );
+            }
+          }
         }
       }
 
-      user.received = received;
-      user.scannedByUser = scannedByUser;
+      user.signatureCount = signatureCount;
 
-      // Only add user to power users if he*she had at least x signatures
-      if (received > signaturesMinimum || scannedByUser > signaturesMinimum) {
+      // Only add user to power users if he*she had at least x signatures for one campaign
+      if (isPowerUser(user, signaturesMinimum)) {
         powerUsers.push(user);
       }
     }
-
-    // Sort power users array by received signatures
-    powerUsers.sort((user1, user2) => user2.received - user1.received);
 
     return {
       statusCode: 200,
@@ -75,4 +81,25 @@ module.exports.handler = async event => {
     console.log('error while getting power users', error);
     errorResponse('error while getting power users');
   }
+};
+
+// Checks if a user is power user by checking if user has more than x
+// signatures for one campaign.
+// Function also removes campaigns with less signatures from user
+const isPowerUser = (user, signaturesMinimum) => {
+  let returnValue = false;
+
+  // Iterate over keys in signatureCount and check if one campaign is more than minimum
+  for (let campaign in user.signatureCount) {
+    if (
+      user.signatureCount[campaign].received > signaturesMinimum ||
+      user.signatureCount[campaign].scannedByUser > signaturesMinimum
+    ) {
+      returnValue = true;
+    } else {
+      delete user.signatureCount[campaign];
+    }
+  }
+
+  return returnValue;
 };
