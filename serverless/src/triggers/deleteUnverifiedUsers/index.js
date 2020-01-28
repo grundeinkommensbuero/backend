8f51d3cd9d6c1cbbd5029383b61a94117a34e505
Valioesi <Vali_Schagerl@web.de>
2020-01-28 16:58:35 +0100

    added serverless config for deleting unverified

--- serverless/src/triggers/deleteUnverifiedUsers/index.js
+++ serverless/src/triggers/deleteUnverifiedUsers/index.js
@@ -1,7 +1,9 @@
 const AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
+const cognito = new AWS.CognitoIdentityServiceProvider();
 const { getAllUnverifiedCognitoUsers } = require('../../shared/users');
 const tableName = process.env.USERS_TABLE_NAME;
+const userPoolId = process.env.USER_POOL_ID;
 
 module.exports.handler = async event => {
   try {
@@ -14,50 +16,45 @@ module.exports.handler = async event => {
 };
 
 const deleteUsers = async () => {
-  try {
-    //get all users, which are not verified from user pool
-    const unverifiedCognitoUsers = await getAllUnverifiedCognitoUsers();
+  //get all users, which are not verified from user pool
+  const unverifiedCognitoUsers = await getAllUnverifiedCognitoUsers();
 
-    //filter users to check if the creation of the user was more than
-    //x days ago
-    const date = new Date();
-    const tenDays = 10 * 24 * 60 * 60 * 1000;
-    const filteredUsers = unverifiedCognitoUsers.filter(
-      user => date - user.UserCreateDate > tenDays
-    );
+  //filter users to check if the creation of the user was more than
+  //x days ago
+  const date = new Date();
+  const tenDays = 10 * 24 * 60 * 60 * 1000;
+  const filteredUsers = unverifiedCognitoUsers.filter(
+    user => date - user.UserCreateDate > tenDays
+  );
 
-    console.log(
-      'not verified and it has been x days count:',
-      filteredUsers.length
-    );
+  console.log(
+    'not verified and it has been x days count:',
+    filteredUsers.length
+  );
 
-    for (let user of filteredUsers) {
-      try {
-        await deleteUserInCognito(user);
-        await deleteUserInDynamo(user);
-      } catch (error) {
-        console.log('error deleting user', error);
-        break;
-      }
+  for (let user of filteredUsers) {
+    try {
+      await deleteUserInCognito(user);
+      await deleteUserInDynamo(user);
+    } catch (error) {
+      console.log('error deleting user', error);
+      break;
     }
-  } catch (error) {
-    console.log('error', error);
   }
-  return;
 };
 
 const deleteUserInCognito = user => {
-  console.log('deleting user in cognito');
+  console.log('deleting user in cognito', user.Username);
   var params = {
     UserPoolId: userPoolId,
     Username: user.Username, //Username is the id of cognito
   };
 
-  return CognitoIdentityServiceProvider.adminDeleteUser(params).promise();
+  return cognito.adminDeleteUser(params).promise();
 };
 
 const deleteUserInDynamo = user => {
-  console.log('deleting user in dynamo');
+  console.log('deleting user in dynamo', user.Username);
   const params = {
     TableName: tableName,
     Key: {
