4ded10415028996d690e5cd52b666c694cc00bb8
Valioesi <Vali_Schagerl@web.de>
2020-02-06 10:16:07 +0100

    refactor: not creating user in cognito trigger

--- serverless/src/api/pledges/createPledge/index.js
+++ serverless/src/api/pledges/createPledge/index.js
@@ -1,6 +1,10 @@
-const { getUser, getUserByMail } = require('../../../shared/users');
+const AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
+const { constructCampaignId } = require('../../shared/utils');
+const { getUser } = require('../../../shared/users');
 const { errorResponse } = require('../../../shared/apiResponse');
-const { savePledge } = require('../../../shared/pledges');
+
+const tableName = process.env.USERS_TABLE_NAME;
 
 module.exports.handler = async event => {
   try {
@@ -13,53 +17,21 @@ module.exports.handler = async event => {
         return errorResponse(400, 'One or more parameters are missing');
       }
 
-      //request body might have email or user id
-      let userId;
-      let user;
-      if ('userId' in requestBody) {
-        userId = requestBody.userId;
-        try {
-          //check if there is a user with the passed user id
-          const result = await getUser(userId);
-
-          console.log('user', result);
-          //if user does not have Item as property, there was no user found
-          if (!('Item' in result) || typeof result.Item === 'undefined') {
-            return errorResponse(404, 'No user found with the passed user id');
-          }
-
-          //we later need the user object
-          user = result.Item;
-        } catch (error) {
-          console.log(error);
-          return errorResponse(500, 'Error while getting user', error);
-        }
-      } else if ('email' in requestBody) {
-        //in case the api only got the email instead of the id we need to get the user id from the db
-        try {
-          const result = await getUserByMail(requestBody.email);
-
-          if (result.Count === 0) {
-            return errorResponse(404, 'No user found with the passed email');
-          } else {
-            //we later need the user object and id
-            user = result.Items[0];
-            userId = user.cognitoId;
-          }
-        } catch (error) {
-          console.log(error);
-          return errorResponse(500, 'Error while getting user by email', error);
-        }
-      }
-
-      // check if a pledge was already made
-      if ('pledges' in user) {
-        return errorResponse(401, 'A pledge for this user was already made');
-      }
-
-      // if no pledge was made, proceed...
       try {
-        await savePledge(userId, requestBody);
+        const { userId } = requestBody;
+
+        //check if there is a user with the passed user id
+        const result = await getUser(userId);
+
+        console.log('user', result);
+        //if user has Item as property, a user was found and therefore already exists
+        if ('Item' in result) {
+          return errorResponse(401, 'A pledge for this user was already made');
+        }
+
+        // if no pledge was made, proceed...
+
+        await savePledge(requestBody);
 
         // saving pledge was successfull, return appropriate json
         return {
@@ -86,7 +58,66 @@ module.exports.handler = async event => {
 
 const validateParams = requestBody => {
   return (
-    ('userId' in requestBody || 'email' in requestBody) &&
+    'userId' in requestBody &&
+    'email' in requestBody &&
     'newsletterConsent' in requestBody
   );
+};
+
+const savePledge = requestBody => {
+  const date = new Date();
+  const timestamp = date.toISOString();
+
+  //check which pledge it is (e.g. pledgeId='brandenburg-1')
+  //create a (nice to later work with) object, which campaign it is
+  const campaign = constructCampaignId(requestBody.pledgeId);
+
+  const pledge = {
+    campaign: campaign,
+    createdAt: timestamp,
+  };
+  // For the state specific pledges a signature count was sent
+  if ('signatureCount' in requestBody) {
+    pledge.signatureCount = requestBody.signatureCount;
+  }
+
+  // For the general "pledge" (more like a newsletter sign up)
+  if ('message' in requestBody && requestBody.message !== '') {
+    pledge.message = requestBody.message;
+  }
+
+  const data = {
+    cognitoId: requestBody.userId,
+    pledge: [pledge],
+    newsletterConsent: {
+      value: requestBody.newsletterConsent,
+      timestamp: timestamp,
+    },
+    createdAt: timestamp,
+  };
+
+  // Check if certain attributes were sent in request and add them...
+  if ('name' in requestBody && requestBody.name !== '') {
+    data.username = requestBody.name;
+  }
+
+  if ('referral' in requestBody) {
+    data.referral = requestBody.referral;
+  }
+
+  if ('zipCode' in requestBody) {
+    data.zipCode = requestBody.zipCode;
+  }
+
+  // If city is the request body we add it (is the case for general pledge)
+  if ('city' in requestBody && requestBody.city !== '') {
+    data.city = requestBody.city;
+  }
+
+  return ddb
+    .put({
+      TableName: tableName,
+      Item: data,
+    })
+    .promise();
 };
