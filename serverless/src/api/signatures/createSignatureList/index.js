af170d06bef39e3a98dbf0fd678f7ac303972b75
Timur Çelikel <me@timurcelikel.com>
2020-02-11 17:20:15 +0100

    createSignatureList: add qr code links for the next states

--- serverless/src/api/signatures/createSignatureList/index.js
+++ serverless/src/api/signatures/createSignatureList/index.js
@@ -23,6 +23,9 @@ const responseHeaders = {
 const qrCodeUrls = {
   'schleswig-holstein': 'https://xbge.de/qr/sh/?listId=',
   brandenburg: 'https://xbge.de/qr/bb/?listId=',
+  hamburg: 'https://xbge.de/qr/hh/?listId=',
+  bremen: 'https://xbge.de/qr/hb/?listId=',
+  berlin: 'https://xbge.de/qr/b/?listId=',
   default: 'https://xbge.de/qr/default/?listId=',
 };
 
