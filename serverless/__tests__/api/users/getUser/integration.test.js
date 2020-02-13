bb65082a957a8d5c5f2b975facfab0c6aace9851
Valioesi <Vali_Schagerl@web.de>
2020-02-13 11:13:35 +0100

    getUsers endpoint

---
+++ serverless/__tests__/api/users/getUser/integration.test.js
@@ -0,0 +1,20 @@
+const { INVOKE_URL } = require('../../../testConfig');
+const fetch = require('node-fetch');
+const userId = '53b95dd2-74b8-49f4-abeb-add9c950c7d9';
+
+describe('getUser api test', () => {
+  it('should be able to get user', async () => {
+    const request = {
+      method: 'GET',
+      mode: 'cors',
+    };
+
+    const response = await fetch(`${INVOKE_URL}/users/${userId}`, request);
+    const json = await response.json();
+
+    expect(json).toHaveProperty('user');
+    expect(json.user).toHaveProperty('userId');
+
+    expect(response.status).toEqual(200);
+  });
+});
