689ce8491cef5d0a1650e947f8f4468f9dcf93d9
Valioesi <Vali_Schagerl@web.de>
2020-02-24 14:53:16 +0100

    endpoint to get user data

--- serverless/src/api/users/getUser/index.js
+++ serverless/src/api/users/getUser/index.js
@@ -1,4 +1,3 @@
-const AWS = require('aws-sdk');
 const { getUser } = require('../../../shared/users');
 const { errorResponse } = require('../../../shared/apiResponse');
 
@@ -11,23 +10,30 @@ module.exports.handler = async event => {
   //get user id from path parameter
   const userId = event.pathParameters.userId;
   try {
-    const user = await getUser(userId);
-    console.log('user', user);
+    const result = await getUser(userId);
     //if user does not have Item as property, there was no user found
-    if (!('Item' in user) || typeof user.Item === 'undefined') {
-      return errorResponse(400, 'No user found with the passed user id');
+    if (!('Item' in result) || typeof result.Item === 'undefined') {
+      return errorResponse(404, 'No user found with the passed user id');
     }
+
+    const user = result.Item;
 
     // return user
     return {
       statusCode: 200,
       body: JSON.stringify({
-        user: { email: user.Item.email, cognitoId: user.Item.cognitoId },
+        user: {
+          profilePictures: user.profilePictures,
+          questions: user.questions,
+          hasZipCode: 'zipCode' in user && user.zipCode !== 'empty',
+          username: user.username,
+        },
       }),
       headers: responseHeaders,
       isBase64Encoded: false,
     };
   } catch (error) {
+    console.log('Error', error);
     return errorResponse(500, 'Error while getting user from table', error);
   }
 };
