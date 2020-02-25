ca65020c9ca5aa6ab4a9cb101b2e435ed5ce4970
Valioesi <Vali_Schagerl@web.de>
2020-02-25 17:54:17 +0100

    get users with most recent questions

--- serverless/src/shared/zipCodeMatcher/index.js
+++ serverless/src/shared/zipCodeMatcher/index.js
@@ -2,6 +2,7 @@ const data = require('./data').data;
 
 const getStateByZipCode = zipCode => {
   const item = data.find(entry => zipCode.startsWith(entry.plz));
+
   if (typeof item !== 'undefined') {
     return item.bundesland;
   } else {
@@ -11,10 +12,23 @@ const getStateByZipCode = zipCode => {
 
 const getZipCodeByCity = city => {
   const item = data.find(entry => city === entry.ort);
+
   if (typeof item !== 'undefined') {
     return item.plz.toString();
   } else {
     return undefined;
   }
 };
-module.exports = { getStateByZipCode, getZipCodeByCity };
+
+const getCityByZipCode = zipCode => {
+  console.log('zip code', zipCode);
+  const item = data.find(entry => zipCode.startsWith(entry.plz));
+
+  if (typeof item !== 'undefined') {
+    return item.ort;
+  } else {
+    return undefined;
+  }
+};
+
+module.exports = { getStateByZipCode, getZipCodeByCity, getCityByZipCode };
