bcaa62d0b3a38a40adc1516c9170872ddf6d0c84
Valioesi <Vali_Schagerl@web.de>
2020-03-03 11:06:19 +0100

    specify various user counts

--- serverless/src/api/users/getUserCount/analyseUsers.js
+++ serverless/src/api/users/getUserCount/analyseUsers.js
@@ -19,79 +19,124 @@ module.exports.analyseUsers = async () => {
   console.log('not verified count', unverifiedCognitoUsers.length);
 
   //go through users to sum up pledged signatures
-  const campaignStats = {};
+  const campaignStats = {
+    totalCount: { verifiedUsers: 0, unverifiedUsers: 0 },
+  };
+
   for (let user of users) {
     //check if the user is verified
     let verified = isVerified(user, unverifiedCognitoUsers);
 
-    //if user is not verified we do not need to count the signatures
-    if ('pledges' in user) {
-      //go through pledges of this users
-      for (let pledge of user.pledges) {
-        const campaign = pledge.campaign.code;
+    let newsletterConsent =
+      'newsletterConsent' in user && user.newsletterConsent.value;
 
-        //initialize object for this pledge;
-        if (!(campaign in campaignStats)) {
-          campaignStats[campaign] = {
-            verifiedUsers: { count: 0, signatures: 0 },
-            unverifiedUsers: { count: 0 },
-            usersWithNewsletterConsent: { count: 0, signatures: 0 },
-            pledgesMap: {}, // how many people have pledged x signatures
-            // powerUsers: [],
-          };
-        }
+    let campaignsOfUser = [];
 
-        //the users, wo were migrated are not in cognito
-        if (verified || user.fromBackup) {
-          campaignStats[campaign].verifiedUsers.count++;
-          let newsletterConsent = false;
-          //count the newsletter consents for all verified users
-          if ('newsletterConsent' in user && user.newsletterConsent.value) {
-            campaignStats[campaign].usersWithNewsletterConsent.count++;
-            newsletterConsent = true;
+    if (verified) {
+      //if user is not verified we do not need to count the signatures
+      if ('pledges' in user) {
+        //go through pledges of this users
+        for (let pledge of user.pledges) {
+          const campaign = pledge.campaign.code;
+
+          campaignsOfUser.push(campaign);
+
+          //initialize object for this pledge;
+          if (!(campaign in campaignStats)) {
+            campaignStats[campaign] = initiateCampaignStats();
           }
 
-          //count the pledged signatures for all verified users
-          if ('signatureCount' in pledge) {
-            const signatureCount = parseInt(pledge.signatureCount);
-            if (!isNaN(signatureCount)) {
-              //if the newsletter consent was given, also add it up for those users
-              if (newsletterConsent) {
+          //the users, wo were migrated are not in cognito
+          if (verified || user.fromBackup) {
+            campaignStats[campaign].verifiedUsers.pledges++;
+
+            //count the pledged signatures for all verified users
+            if ('signatureCount' in pledge) {
+              const signatureCount = parseInt(pledge.signatureCount);
+
+              if (!isNaN(signatureCount)) {
+                //if the newsletter consent was given, also add it up for those users
+                if (newsletterConsent) {
+                  campaignStats[
+                    campaign
+                  ].usersWithNewsletterConsent.signatures += signatureCount;
+                }
+
+                //otherwise only add it for the (only) verified users
                 campaignStats[
                   campaign
-                ].usersWithNewsletterConsent.signatures += signatureCount;
+                ].verifiedUsers.signatures += signatureCount;
+
+                // Check if the signatureCount is already in the pledge map
+                if (!(signatureCount in campaignStats[campaign].pledgesMap)) {
+                  campaignStats[campaign].pledgesMap[signatureCount] = 0;
+                }
+
+                campaignStats[campaign].pledgesMap[signatureCount]++;
               }
-              //otherwise only add it for the (only) verified users
-              campaignStats[
-                campaign
-              ].verifiedUsers.signatures += signatureCount;
-
-              // Check if the signatureCount is already in the pledge map
-              if (!(signatureCount in campaignStats[campaign].pledgesMap)) {
-                campaignStats[campaign].pledgesMap[signatureCount] = 0;
-              }
-
-              campaignStats[campaign].pledgesMap[signatureCount]++;
-
-              //generate a list of power users
-              //(20+ pledged signatures or wants to collect in public spaces)
-              /*  don't really need it right now
-             if (
-                signatureCount >= 20 ||
-                pledge.wouldCollectSignaturesInPublicSpaces
-              ) {
-                campaignStats[campaign].powerUsers.push(user);
-              } */
             }
+          }
+        }
+      }
+
+      // Also check if there is campaign code which was added when downloading list
+      if ('signatureCampaigns' in user) {
+        for (let campaign of user.signatureCampaigns) {
+          campaignsOfUser.push(campaign.code);
+
+          if (!(campaign.code in campaignStats)) {
+            campaignStats[campaign.code] = initiateCampaignStats();
+          }
+
+          if (newsletterConsent) {
+            campaignStats[campaign.code].usersWithNewsletterConsent
+              .downloaders++;
+          }
+
+          campaignStats[campaign.code].verifiedUsers.downloaders++;
+        }
+      }
+
+      for (let campaign of campaignsOfUser) {
+        if (verified) {
+          // General count for this campaign
+          campaignStats[campaign].verifiedUsers.count++;
+
+          if (newsletterConsent) {
+            campaignStats[campaign].usersWithNewsletterConsent.count++;
           }
         } else {
           campaignStats[campaign].unverifiedUsers.count++;
         }
       }
+
+      // Total count
+      campaignStats.totalCount.verifiedUsers++;
+    } else {
+      campaignStats.totalCount.unverifiedUsers++;
     }
   }
 
   return campaignStats;
+};
+
+const initiateCampaignStats = () => {
+  return {
+    verifiedUsers: {
+      count: 0,
+      signatures: 0,
+      pledges: 0,
+      downloaders: 0,
+    },
+    unverifiedUsers: { count: 0 },
+    usersWithNewsletterConsent: {
+      count: 0,
+      signatures: 0,
+      pledges: 0,
+      downloaders: 0,
+    },
+    pledgesMap: {}, // how many people have pledged x signatures
+  };
 };
 
 const getAllUsersWithoutNewsletterConsent = () => {
