a47c2c8963fa414bff278a203987fc02d2f6bb44
Valioesi <Vali_Schagerl@web.de>
2020-03-23 13:14:35 +0100

    util script to count new sign ups

---
+++ utils/analyseData/signUps/index.js
@@ -0,0 +1,16 @@
+const { getAllUsers } = require('../../shared/users/getUsers');
+const CONFIG = require('../../config');
+
+const getNewUserCountSinceDate = async date => {
+  const users = await getAllUsers(CONFIG.PROD_USERS_TABLE_NAME);
+
+  console.log('user count', users.length);
+
+  date = new Date(date);
+
+  newUsers = users.filter(user => new Date(user.createdAt) > date);
+
+  console.log(`user count since ${date}`, newUsers.length);
+};
+
+getNewUserCountSinceDate('2020-03-19');
