7c562be5dc3e65a664bc70ef1ac32c10bc01310c
Timur Çelikel <me@timurcelikel.com>
2020-04-29 10:13:05 +0200

    createSignatureList: function to create letters more easily

--- serverless/src/api/signatures/createSignatureList/test.js
+++ serverless/src/api/signatures/createSignatureList/test.js
@@ -1,15 +1,32 @@
 const fs = require('fs');
 
 const generatePdf = require('./createPDF');
+const createPDFLetter = require('./createPDFLetter');
 
 const CODE = '0123456789';
 const URL = 'https://xbge.de/qr/hh/?listId=';
 
-generatePdf(URL, CODE, 'COMBINED', 'berlin-1', {
-  name: 'Max Musterfrau',
-  street: 'Am Fleet 2',
-  zipCode: '21333',
-  city: 'Hamburg',
+// generatePdf(URL, CODE, 'ANSCHREIBEN_GENERAL', 'hamburg-1', {
+//   name: 'Max Musterfrau',
+//   street: 'Am Fleet 2',
+//   zipCode: '21333',
+//   city: 'Hamburg',
+// }).then(pdfBytes => {
+//   fs.writeFileSync('./test-serienbrief.pdf', pdfBytes);
+// });
+
+createPDFLetter({
+  url: URL,
+  code: CODE,
+  campaignCode: 'brandenburg-1',
+  address: {
+    name: 'Max Musterfrau',
+    street: 'Am Fleet 2',
+    zipCode: '21333',
+    city: 'Hamburg',
+  },
+  needsMailMissingAddition: false,
+  listCount: 1,
 }).then(pdfBytes => {
   fs.writeFileSync('./test-serienbrief.pdf', pdfBytes);
 });
