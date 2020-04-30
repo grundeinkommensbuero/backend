51a72b52e2ca734617f74bcbcfc1d8e53d9d8e7e
Valioesi <Vali_Schagerl@web.de>
2020-04-30 21:38:47 +0200

    Survey: remove answerId + only create same answer once

--- serverless/__tests__/api/users/createSurveyAnswer/integration.test.js
+++ serverless/__tests__/api/users/createSurveyAnswer/integration.test.js
@@ -1,17 +1,17 @@
 const { INVOKE_URL } = require('../../../testConfig');
 const fetch = require('node-fetch');
+const randomWords = require('random-words');
 
 const userId = '53b95dd2-74b8-49f4-abeb-add9c950c7d9';
 
 describe('createSurveyAnswer api test', () => {
-  it('should be able to update user', async () => {
+  it('should be able to create survey answer', async () => {
     const request = {
       method: 'POST',
       mode: 'cors',
       body: JSON.stringify({
-        answerId: 1,
         answer: 'answer-1',
-        surveyCode: 'survey-1',
+        surveyCode: `${randomWords()}-1`,
       }),
     };
 
@@ -23,12 +23,29 @@ describe('createSurveyAnswer api test', () => {
     expect(response.status).toEqual(201);
   });
 
+  it('should not be able to create survey answer for same surveys', async () => {
+    const request = {
+      method: 'POST',
+      mode: 'cors',
+      body: JSON.stringify({
+        answer: 'answer-1',
+        surveyCode: 'survey-1',
+      }),
+    };
+
+    const response = await fetch(
+      `${INVOKE_URL}/users/${userId}/surveys`,
+      request
+    );
+
+    expect(response.status).toEqual(401);
+  });
+
   it('should have missing params', async () => {
     const request = {
       method: 'POST',
       mode: 'cors',
       body: JSON.stringify({
-        answerId: 1,
         answer: 'answer-1',
       }),
     };
