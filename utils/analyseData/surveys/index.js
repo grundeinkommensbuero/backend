92da0912bab1d764516a180ad0d8d37bef3fecca
Valioesi <Vali_Schagerl@web.de>
2020-05-26 12:44:42 +0200

    Script to analyse survey results

---
+++ utils/analyseData/surveys/index.js
@@ -0,0 +1,26 @@
+const CONFIG = require('../../config');
+const { getUsersWithSurvey } = require('../../shared/users/getUsers');
+const tableName = CONFIG.PROD_USERS_TABLE_NAME;
+
+const getSurveyResults = async () => {
+  const users = await getUsersWithSurvey(tableName);
+
+  const surveyResults = {};
+  for (let user of users) {
+    for (let survey of user.surveys) {
+      if (!(survey.code in surveyResults)) {
+        surveyResults[survey.code] = {};
+      }
+
+      if (!(survey.answer in surveyResults[survey.code])) {
+        surveyResults[survey.code][survey.answer] = 0;
+      }
+
+      surveyResults[survey.code][survey.answer]++;
+    }
+  }
+
+  console.log(surveyResults);
+};
+
+getSurveyResults();
