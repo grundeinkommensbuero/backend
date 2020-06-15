392c5e0462d358e94b82a1deb2ef185e0bd0256d
Valioesi <Vali_Schagerl@web.de>
2020-06-15 12:42:03 +0200

    Update Mailjet upon change in signatures table

--- serverless/src/triggers/mailjet/updateMailjetContact/index.js
+++ serverless/src/triggers/mailjet/updateMailjetContact/index.js
@@ -6,47 +6,67 @@ const { getUser, getCognitoUser } = require('../../../shared/users');
 module.exports.handler = async event => {
   try {
     // Only run the script if the environment is prod
-    if (process.env.STAGE === 'prod' || true) {
+    if (process.env.STAGE === 'prod') {
       for (let record of event.Records) {
         console.log({ record });
-        const userId = record.dynamodb.Keys.cognitoId.S;
+
+        // Check whether the stream came from users or signatures table
+        // (depending on the key)
+        let cameFromUsersTable = false;
+        let userId;
+        if ('cognitoId' in record.dynamodb.Keys) {
+          cameFromUsersTable = true;
+          userId = record.dynamodb.Keys.cognitoId.S;
+        } else {
+          // came from signatures table stream
+          if (record.eventName !== 'REMOVE') {
+            userId = record.dynamodb.NewImage.userId.S;
+          }
+        }
 
         if (record.eventName === 'REMOVE') {
-          //record was removed
-          //which is why we want to remove the mailjet contact as well
+          // record was removed
+          // which is why we want to remove the mailjet contact as well
 
-          console.log('record was removed', record);
-          try {
-            const email = record.dynamodb.OldImage.email.S;
+          if (cameFromUsersTable) {
+            console.log('record was removed', record);
+            try {
+              const email = record.dynamodb.OldImage.email.S;
 
-            await deleteMailjetContact(email);
-            console.log('success deleting contact');
-          } catch (error) {
-            console.log('error deleting contact', error);
+              await deleteMailjetContact(email);
+              console.log('success deleting contact');
+            } catch (error) {
+              console.log('error deleting contact', error);
+            }
           }
         } else {
           // Record was updated
-          // We want to get the normal dynamo record to not have to deal
-          // with the weird data format of the stream
-          // Get signature lists of this user and add it to user object
-          const result = await getUser(userId);
+          if (userId !== 'anonymous') {
+            console.log('about to update user', userId);
+            console.log('came from users table', cameFromUsersTable);
 
-          if ('Item' in result) {
-            const user = result.Item;
-            user.signatureLists = await getSignatureListsOfUser(user.cognitoId);
+            // We want to get the normal dynamo record to not have to deal
+            // with the weird data format of the stream
+            const result = await getUser(userId);
 
-            // We also need to get the cognito user to check if the user is verified
-            const { UserStatus } = await getCognitoUser(user.cognitoId);
-            const verified = UserStatus === 'CONFIRMED';
+            if ('Item' in result) {
+              const user = result.Item;
+              // Get signature lists of this user and add it to user object
+              user.signatureLists = await getSignatureListsOfUser(userId);
 
-            await syncMailjetContact(user, verified);
+              // We also need to get the cognito user to check if the user is verified
+              const { UserStatus } = await getCognitoUser(userId);
+              const verified = UserStatus === 'CONFIRMED';
+
+              await syncMailjetContact(user, verified);
+            }
           }
         }
       }
       return `Successfully processed ${event.Records.length} records.`;
     }
   } catch (error) {
-    console.log('error while updating pinpoint', error);
+    console.log('error while updating mailjet', error);
   }
 
   return;
