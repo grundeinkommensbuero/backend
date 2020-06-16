f78887a56181e310a05feb904ba56354bdcd24bb
Valioesi <Vali_Schagerl@web.de>
2020-06-16 11:34:21 +0200

    Only run onVerification in prod stage

--- serverless/src/triggers/auth/onVerification/index.js
+++ serverless/src/triggers/auth/onVerification/index.js
@@ -5,15 +5,18 @@ const { getUser } = require('../../../shared/users');
 // We want to update mailjet accordingly
 exports.handler = async event => {
   try {
-    // userName is the user id
-    const userId = event.userName;
+    // Only run the script if the environment is prod
+    if (process.env.STAGE === 'prod') {
+      // userName is the user id
+      const userId = event.userName;
 
-    // We don't need to update the whole mailjet contact, but only
-    // the subsription status, which is why we need the newsletter consent
-    const result = await getUser(userId);
+      // We don't need to update the whole mailjet contact, but only
+      // the subsription status, which is why we need the newsletter consent
+      const result = await getUser(userId);
 
-    if ('Item' in result) {
-      await updateMailjetSubscription(result.Item, true);
+      if ('Item' in result) {
+        await updateMailjetSubscription(result.Item, true);
+      }
     }
   } catch (error) {
     console.log('Error updating mailjet subscription', error);
