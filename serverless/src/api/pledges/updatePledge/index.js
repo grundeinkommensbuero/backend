91e21be47504c166df7d99f9dbf9e33c42082dd4
Valioesi <Vali_Schagerl@web.de>
2020-07-08 17:17:35 +0200

    Refactored creating and updating users and pledges

--- serverless/src/api/pledges/updatePledge/index.js
+++ serverless/src/api/pledges/updatePledge/index.js
@@ -1,7 +1,8 @@
 const AWS = require('aws-sdk');
-const { constructCampaignId } = require('../../../shared/utils');
 const { getUser } = require('../../../shared/users');
+const { savePledge } = require('../../../shared/pledges');
 const { errorResponse } = require('../../../shared/apiResponse');
+const { constructCampaignId } = require('../../../shared/utils');
 
 const ddb = new AWS.DynamoDB.DocumentClient();
 const tableName = process.env.USERS_TABLE_NAME;
@@ -24,8 +25,8 @@ module.exports.handler = async event => {
     }
 
     try {
-      // check if there is a user with the passed user id
       const { userId } = event.pathParameters;
+      const { pledgeId } = requestBody;
       const result = await getUser(userId);
 
       console.log('user', result);
@@ -35,7 +36,25 @@ module.exports.handler = async event => {
         return errorResponse(404, 'No user found with the passed user id');
       }
 
-      await savePledge(userId, requestBody);
+      const { pledges } = result.Item;
+
+      // Check if this pledge already exists
+      if (typeof pledges !== 'undefined') {
+        const index = pledges.findIndex(
+          pledge => pledge.campaign.code === pledgeId
+        );
+
+        if (index !== -1) {
+          // Pledge already exists
+          await updatePledge(userId, pledges, index, requestBody);
+        } else {
+          // This pledge does not exist
+          await savePledge(userId, requestBody);
+        }
+      } else {
+        // No pledges exist
+        await savePledge(userId, requestBody);
+      }
 
       // saving pledge was successful, return appropriate json
       return {
@@ -57,7 +76,7 @@ module.exports.handler = async event => {
 };
 
 const validateParams = (event, requestBody) => {
-  return 'userId' in event.pathParameters && 'newsletterConsent' in requestBody;
+  return 'userId' in event.pathParameters && 'pledgeId' in requestBody;
 };
 
 const isAuthorized = event => {
@@ -66,68 +85,39 @@ const isAuthorized = event => {
   );
 };
 
-const savePledge = (userId, requestBody) => {
-  const date = new Date();
-  const timestamp = date.toISOString();
+const updatePledge = (
+  userId,
+  pledges,
+  index,
+  { pledgeId, message, signatureCount }
+) => {
+  const timestamp = new Date().toISOString();
 
   // check which pledge it is (e.g. pledgeId='brandenburg-1')
   // create a (nice to later work with) object, which campaign it is
-  const campaign = constructCampaignId(requestBody.pledgeId);
+  const campaign = constructCampaignId(pledgeId);
 
   const pledge = {
     campaign,
     createdAt: timestamp,
+    // For the general "pledge" (more like a newsletter sign up)
+    message,
+    // For the state specific pledges a signature count was sent
+    signatureCount,
   };
-  // For the state specific pledges a signature count was sent
-  if ('signatureCount' in requestBody) {
-    pledge.signatureCount = requestBody.signatureCount;
-  }
 
-  // For the general "pledge" (more like a newsletter sign up)
-  if ('message' in requestBody && requestBody.message !== '') {
-    pledge.message = requestBody.message;
-  }
+  // Overwrite old pledge with new one
+  pledges[index] = pledge;
 
-  const data = {
-    // needs to be array because append_list works with an array
-    ':pledge': [pledge],
-    ':newsletterConsent': {
-      value: requestBody.newsletterConsent,
-      timestamp,
+  const params = {
+    TableName: tableName,
+    Key: { cognitoId: userId },
+    UpdateExpression: 'SET pledges =  :pledges',
+    ExpressionAttributeValues: {
+      ':pledges': pledges,
     },
-    ':emptyList': [],
+    ReturnValues: 'UPDATED_NEW',
   };
 
-  // If city is the request body we add it (is the case for general pledge)
-  if ('city' in requestBody && requestBody.city !== '') {
-    data[':city'] = requestBody.city;
-  }
-
-  if ('zipCode' in requestBody) {
-    data[':zipCode'] = requestBody.zipCode;
-  }
-
-  if ('name' in requestBody && requestBody.name !== '') {
-    data[':username'] = requestBody.name;
-  }
-
-  // if there is no pledges key yet we initiate it with an array,
-  // otherwise we add the pledge to the array
-  const updateExpression = `
-  set ${':city' in data ? 'city = :city,' : ''}
-  ${':username' in data ? 'username = :username,' : ''}
-  ${':zipCode' in data ? 'zipCode = :zipCode,' : ''}
-  pledges = list_append(if_not_exists(pledges, :emptyList), :pledge),
-  newsletterConsent = :newsletterConsent
-  `;
-
-  return ddb
-    .update({
-      TableName: tableName,
-      Key: { cognitoId: userId },
-      UpdateExpression: updateExpression,
-      ExpressionAttributeValues: data,
-      ReturnValues: 'UPDATED_NEW',
-    })
-    .promise();
+  return ddb.update(params).promise();
 };
