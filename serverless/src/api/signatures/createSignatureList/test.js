69de2b4edd095ab66b4eb1713099e65cac934146
Valioesi <Vali_Schagerl@web.de>
2020-08-13 17:19:04 +0200

    Prepared everything for lists via mail for bremen

--- serverless/src/api/signatures/createSignatureList/test.js
+++ serverless/src/api/signatures/createSignatureList/test.js
@@ -6,40 +6,33 @@ const createPDFLetter = require('./createPDFLetter');
 const CODE = '0123456789';
 const URL = 'https://xbge.de/qr/hh/?listId=';
 
-generatePdf(URL, CODE, 'COMBINED', 'bremen-1').then(pdfBytes => {
-  fs.writeFileSync('./test-dibb.pdf', pdfBytes);
-});
-
-// createPDFLetter({
-//   url: URL,
-//   code: CODE,
-//   lists: [
-//     {
-//       campaignCode: 'brandenburg-1',
-//       state: 'brandenburg',
-//       listCount: 1,
-//       code: '43234',
-//     },
-//     {
-//       campaignCode: 'berlin-1',
-//       state: 'berlin',
-//       listCount: 2,
-//       code: '12345',
-//     },
-//     {
-//       campaignCode: 'schleswig-holstein-1',
-//       state: 'schleswig-holstein',
-//       listCount: 2,
-//       code: '12345',
-//     },
-//   ],
-//   address: {
-//     name: 'Max Musterfrau',
-//     street: 'Am Fleet 2',
-//     zipCode: '21333',
-//     city: 'Hamburg',
-//   },
-//   needsMailMissingAddition: true,
-// }).then(pdfBytes => {
-//   fs.writeFileSync('./test-serienbrief.pdf', pdfBytes);
+// generatePdf(URL, CODE, 'COMBINED', 'bremen-1').then(pdfBytes => {
+//   fs.writeFileSync('./test-dibb.pdf', pdfBytes);
 // });
+
+createPDFLetter({
+  url: URL,
+  code: CODE,
+  lists: [
+    {
+      campaignCode: 'bremen-1',
+      state: 'bremen',
+      listCount: 1,
+      code: '43234',
+    },
+    {
+      campaignCode: 'bremen-1',
+      state: 'bremen',
+      listCount: 1,
+      code: '12234',
+    },
+  ],
+  address: {
+    name: 'Max Musterfrau',
+    street: 'Am Fleet 2',
+    zipCode: '21333',
+    city: 'Hamburg',
+  },
+}).then(pdfBytes => {
+  fs.writeFileSync('./test-serienbrief.pdf', pdfBytes);
+});
