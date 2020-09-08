a0aaaa9488da2930354bcd5e34e210f902d219d3
Valioesi <Vali_Schagerl@web.de>
2020-09-08 13:57:32 +0200

    Anonymize email: star for every char

--- serverless/src/api/users/getUser/utils.js
+++ serverless/src/api/users/getUser/utils.js
@@ -1,14 +1,21 @@
 module.exports.anonymizeEmail = email => {
   if (!email) return undefined;
   const [username, domain] = email.split('@');
-  const usernameFirstLetter = username[0];
-  const usernameLastLetter = username[username.length - 1];
+
+  // We keep the first and last char before @ and replace every other char with *
+  const anonymizedUsername = `${username[0]}${'*'.repeat(username.length - 2)}${
+    username[username.length - 1]
+  }`;
 
   const domainParts = domain.split('.');
   const tld = domainParts[domainParts.length - 1];
   // Join all of the subdomain to one string
   const subdomains = domainParts.slice(0, domainParts.length - 1).join('.');
-  const firstSubdomainLetter = subdomains[0];
-  const lastSubdomainLetter = subdomains[subdomains.length - 1];
-  return `${usernameFirstLetter}***${usernameLastLetter}@${firstSubdomainLetter}***${lastSubdomainLetter}.${tld}`;
+
+  // We keep the first and last char before @ and replace every other char with *
+  const anonymizedSubdomains = `${subdomains[0]}${'*'.repeat(
+    subdomains.length - 2
+  )}${subdomains[subdomains.length - 1]}`;
+
+  return `${anonymizedUsername}@${anonymizedSubdomains}.${tld}`;
 };
