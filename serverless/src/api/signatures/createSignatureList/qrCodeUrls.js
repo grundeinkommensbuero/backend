92ec68cb82fd58f23cd084a5c4c84ba6dff0ba13
Valioesi <Vali_Schagerl@web.de>
2020-09-16 10:43:23 +0200

    Qr code for dibb should go to self scan

--- serverless/src/api/signatures/createSignatureList/qrCodeUrls.js
+++ serverless/src/api/signatures/createSignatureList/qrCodeUrls.js
@@ -4,6 +4,6 @@ module.exports = {
   hamburg: 'https://xbge.de/qr/hh/?listId=',
   bremen: 'https://xbge.de/qr/hb/?listId=',
   berlin: 'https://xbge.de/qr/b/?listId=',
-  dibb: 'https://xbge.de/',
+  dibb: 'https://xbge.de/qr/bb/?listId=',
   default: 'https://xbge.de/qr/default/?listId=',
 };
