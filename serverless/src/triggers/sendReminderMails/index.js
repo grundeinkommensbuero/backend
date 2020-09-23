0f7d850e576331a4764bb7fc5956782692adb5bd
Valioesi <Vali_Schagerl@web.de>
2020-09-23 17:12:29 +0200

    Send different reminder mail after couple of weeks

--- serverless/src/triggers/sendReminderMails/index.js
+++ serverless/src/triggers/sendReminderMails/index.js
@@ -2,7 +2,7 @@ const {
   analyseSignatureLists,
 } = require('../../api/signatures/getSignatureListCount/analyseSignatureLists');
 const {
-  getNotScannedSignatureLists,
+  getNotReceivedSignatureLists,
   getScansOfUser,
   getSignatureCountOfAllLists,
 } = require('../../shared/signatures');
@@ -13,13 +13,15 @@ const sendMail = require('./sendMail');
 const qrCodeUrls = require('../../shared/signatures/createPdf/qrCodeUrls');
 
 const remindAfter = [2, 6, 10];
+// We send a different mail as final reminders to send in the signatures
+const finalRemindAfter = [14, 21];
 
 module.exports.handler = async event => {
   try {
     // Get general stats
-    // and Get all lists which were not scanned by user or sent yet
+    // and Get all lists which were not received yet
     const [signatureLists, signatureCounts, listCounts] = await Promise.all([
-      getNotScannedSignatureLists(),
+      getNotReceivedSignatureLists(),
       getSignatureCountOfAllLists(),
       analyseSignatureLists(),
     ]);
@@ -35,83 +37,114 @@ module.exports.handler = async event => {
       timestamps.push(timestamp);
     }
 
+    const finalTimestamps = [];
+    for (const days of finalRemindAfter) {
+      const date = new Date();
+      date.setDate(date.getDate() - days);
+
+      // we only want the current day (YYYY-MM-DD), because the lists are saved that way
+      const timestamp = date.toISOString().substring(0, 10);
+      finalTimestamps.push(timestamp);
+    }
+
     // Loop through lists to check if a list was created x days ago
     for (const list of signatureLists) {
-      // We only need to check lists of users and lists which were not part of letter action
-      if (list.userId !== 'anonymous' && !list.manually) {
+      // We only need to check lists of users and lists which were not part of letter action.
+      // Also we want to deactivate the dibb lists for now...
+      if (
+        list.userId !== 'anonymous' &&
+        !list.manually &&
+        list.campaign.code !== 'dibb-1'
+      ) {
         // Check if list was created x days ago, then we send a reminder mail
-        const index = timestamps.indexOf(list.createdAt);
+        let index = timestamps.indexOf(list.createdAt);
+
+        let mailType = null;
+        if (index === -1) {
+          index = finalTimestamps.indexOf(list.createdAt);
+          mailType =
+            index === finalTimestamps.length - 1 ? 'ultimate' : 'penultimate';
+        }
+
         if (index !== -1) {
-          console.log('same date', list.createdAt, timestamps, list);
+          // The last reminder will be different and will be the only one
+          // being sent, if the the list is only not received (but can be scanned by user)
+          if ((!mailType && !('scannedByUser' in list)) || mailType) {
+            console.log('same date', list.createdAt, timestamps, list);
 
-          // Get user from users table to get email
-          const result = await getUser(list.userId);
+            // Get user from users table to get email
+            const result = await getUser(list.userId);
 
-          // the user might have been deleted or does not have
-          // newsletter consent
-          // TODO: also send to people without consent?
-          if (
-            'Item' in result &&
-            'newsletterConsent' in result.Item &&
-            result.Item.newsletterConsent.value
-          ) {
-            const user = result.Item;
+            // the user might have been deleted or does not have
+            // newsletter consent
+            // TODO: also send to people without consent?
+            if (
+              'Item' in result &&
+              'newsletterConsent' in result.Item &&
+              result.Item.newsletterConsent.value
+            ) {
+              const user = result.Item;
 
-            // we should probably check if user has scanned or sent other lists
-            const scans = await getScansOfUser(user, list.campaign.code);
+              // we should probably check if user has scanned or sent other lists
+              const scans = await getScansOfUser(user, list.campaign.code);
 
-            // If the user scanned or sent a list after the latest list was downloaded (also for example a
-            // different list) we don't want to send the reminder
-            let shouldSendMail = true;
-            for (const scan of [
-              ...scans.receivedList,
-              ...scans.scannedByUserList,
-            ]) {
-              console.log(
-                'scan.timestamp',
-                new Date(scan.timestamp.substring(0, 10))
-              );
-              console.log('list.createdAt', new Date(list.createdAt));
-
-              // The issue is than list.createdAt will always be at 0 o'clock,
-              // because the database entry is YYYY-MM-DD. That's why we can't
-              // compare the values super precisely. The following comparison covers e.g. the case:
-              // list a is downloaded on day x and signatures are recorded for that list on
-              // day y, then list b is downloaded afterwards on day y. We still want to send
-              // the reminder mail. So if there is a scan at the same day as a list was downloaded
-              // we send the mail (the scan was for a different list anyway because we only fetched lists
-              // with no scans).
-              if (
-                new Date(scan.timestamp.substring(0, 10)) >
-                new Date(list.createdAt)
-              ) {
-                shouldSendMail = false;
+              // If the user scanned or sent a list after the latest list was downloaded (also for example a
+              // different list) we don't want to send the reminder
+              let shouldSendMail = true;
+              const scanArray = scans.receivedList;
+              // If it is one of the ultimate mails we also want to send one, if the user scanned but
+              // did not send, therefore in that case we don't add the scanned by user scans to compare them with
+              if (!mailType) {
+                scanArray.push(...scans.scannedByUserList);
               }
-            }
 
-            if (shouldSendMail) {
-              // Generate the pdfs to attach to the mail
-              const qrCodeUrl = qrCodeUrls[list.campaign.state]
-                ? qrCodeUrls[list.campaign.state]
-                : qrCodeUrls.default;
+              for (const scan of scanArray) {
+                // The issue is that list.createdAt will always be at 0 o'clock,
+                // because the database entry is YYYY-MM-DD. That's why we can't
+                // compare the values super precisely. The following comparison covers e.g. the case:
+                // list a is downloaded on day x and signatures are recorded for that list on
+                // day y, then list b is downloaded afterwards on day y. We still want to send
+                // the reminder mail. So if there is a scan at the same day as a list was downloaded
+                // we send the mail (the scan was for a different list anyway because we only fetched lists
+                // with no scans).
+                if (
+                  new Date(scan.timestamp.substring(0, 10)) >
+                  new Date(list.createdAt)
+                ) {
+                  shouldSendMail = false;
+                }
+              }
 
-              const attachments = await generateAttachments(
-                mailAttachments[list.campaign.code],
-                qrCodeUrl,
-                list.id,
-                list.campaign.code
-              );
+              if (list.id === '1893799') {
+                console.log('should send mail', shouldSendMail);
+              }
 
-              await sendMail(
-                user,
-                list.id,
-                list.campaign,
-                remindAfter[index],
-                signatureCounts,
-                listCounts,
-                attachments
-              );
-              console.log('success sending mail to', user.email);
+              if (shouldSendMail) {
+                // Generate the pdfs to attach to the mail
+                const qrCodeUrl = qrCodeUrls[list.campaign.state]
+                  ? qrCodeUrls[list.campaign.state]
+                  : qrCodeUrls.default;
+
+                const attachments = await generateAttachments(
+                  mailAttachments[list.campaign.code],
+                  qrCodeUrl,
+                  list.id,
+                  list.campaign.code
+                );
+
+                await sendMail(
+                  user,
+                  list.id,
+                  list.campaign,
+                  mailType ? finalRemindAfter[index] : remindAfter[index],
+                  mailType,
+                  signatureCounts,
+                  listCounts,
+                  scans.scannedByUser,
+                  attachments
+                );
+                console.log('success sending mail to', user.email);
+              }
             }
           }
         }
