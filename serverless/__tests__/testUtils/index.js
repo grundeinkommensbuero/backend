6cdc92516cad0d369527dec6a93745d1bbee1317
Valioesi <Vali_Schagerl@web.de>
2020-10-21 18:24:46 +0200

    Use password instead token for auth while testing

--- serverless/__tests__/testUtils/index.js
+++ serverless/__tests__/testUtils/index.js
@@ -3,8 +3,9 @@ const {
   ADMIN_CLIENT_ID,
   USER_POOL_ID,
   CLIENT_ID,
-  REFRESH_TOKEN,
-  ADMIN_REFRESH_TOKEN,
+  USER_ID,
+  PASSWORD,
+  ADMIN_USER_ID,
 } = require('../testConfig');
 const AWS = require('aws-sdk');
 
@@ -12,25 +13,22 @@ const config = { region: 'eu-central-1' };
 const cognito = new AWS.CognitoIdentityServiceProvider(config);
 
 module.exports.authenticate = async () => {
-  return makeAuthenticationCall(USER_POOL_ID, CLIENT_ID, REFRESH_TOKEN);
+  return makeAuthenticationCall(USER_POOL_ID, CLIENT_ID, USER_ID);
 };
 
 module.exports.authenticateAdmin = async () => {
-  return makeAuthenticationCall(
-    ADMIN_POOL_ID,
-    ADMIN_CLIENT_ID,
-    ADMIN_REFRESH_TOKEN
-  );
+  return makeAuthenticationCall(ADMIN_POOL_ID, ADMIN_CLIENT_ID, ADMIN_USER_ID);
 };
 
-const makeAuthenticationCall = async (poolId, clientId, token) => {
+const makeAuthenticationCall = async (poolId, clientId, userId) => {
   const { AuthenticationResult } = await cognito
     .adminInitiateAuth({
-      AuthFlow: 'REFRESH_TOKEN',
+      AuthFlow: 'ADMIN_USER_PASSWORD_AUTH',
       UserPoolId: poolId,
       ClientId: clientId,
       AuthParameters: {
-        REFRESH_TOKEN: token,
+        USERNAME: userId,
+        PASSWORD,
       },
     })
     .promise();
