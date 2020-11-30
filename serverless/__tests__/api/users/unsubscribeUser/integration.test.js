1ce77e872f8eae0b6b429b611f7a3aebc73ee1e1
Valioesi <Vali_Schagerl@web.de>
2020-11-30 17:16:27 +0100

    Mailjet syncronisation for newsletter settings

--- serverless/__tests__/api/users/unsubscribeUser/integration.test.js
+++ serverless/__tests__/api/users/unsubscribeUser/integration.test.js
@@ -1,15 +1,35 @@
-const { INVOKE_URL_WITHOUT_HTTPS } = require('../../../testConfig');
+const {
+  INVOKE_URL_WITHOUT_HTTPS,
+  DEV_USERS_TABLE,
+} = require('../../../testConfig');
 const { username, password } = require('../../../../basicAuth');
 const fetch = require('node-fetch');
+const AWS = require('aws-sdk');
+const uuid = require('uuid/v4');
+const { getUser } = require('../../../../../utils/shared/users/getUsers');
+
+const config = { region: 'eu-central-1' };
+const ddb = new AWS.DynamoDB.DocumentClient(config);
+
+const userId = uuid();
+const email = 'unsubscribe.test@expedition-grundeinkommen.de';
 
 describe('unsubscribeUser api test', () => {
+  beforeAll(async () => {
+    await createUser();
+  });
+
+  afterAll(async () => {
+    await deleteUser();
+  });
+
   it('should be able to unsubscribe user', async () => {
     const request = {
       method: 'POST',
       mode: 'cors',
       body: JSON.stringify([
         {
-          email: 'valentin@expedition-grundeinkommen.de',
+          email,
         },
       ]),
     };
@@ -19,7 +39,19 @@ describe('unsubscribeUser api test', () => {
       request
     );
 
+    // Get user to check if unsubscribed correctly
+    const { Item: user } = await getUser(DEV_USERS_TABLE, userId);
+
+    let allFalse = true;
+    for (const newsletter of user.customNewsletters) {
+      if (newsletter.value) {
+        allFalse = false;
+      }
+    }
+
     expect(response.status).toEqual(200);
+    expect(user.newsletterConsent.value).toEqual(false);
+    expect(allFalse).toEqual(true);
   });
 
   it('should not find email', async () => {
@@ -79,3 +111,48 @@ describe('unsubscribeUser api test', () => {
     expect(response.status).toEqual(401);
   });
 });
+
+const createUser = () => {
+  const timestamp = new Date().toISOString();
+
+  const params = {
+    TableName: DEV_USERS_TABLE,
+    Item: {
+      cognitoId: userId,
+      email,
+      newsletterConsent: {
+        value: true,
+        timestamp,
+      },
+      customNewsletters: [
+        {
+          extraInfo: true,
+          value: true,
+          name: 'Blub1',
+        },
+        {
+          extraInfo: false,
+          value: true,
+          name: 'Blub2',
+        },
+        {
+          extraInfo: false,
+          value: false,
+          name: 'Blub3',
+        },
+      ],
+    },
+  };
+  return ddb.put(params).promise();
+};
+
+const deleteUser = () => {
+  const params = {
+    TableName: DEV_USERS_TABLE,
+    Key: {
+      cognitoId: userId, // Username is the id of cognito
+    },
+  };
+
+  return ddb.delete(params).promise();
+};
--- serverless/src/api/users/unsubscribeUser/index.js
+++ serverless/src/api/users/unsubscribeUser/index.js
@@ -1,9 +1,15 @@
-const {
-  getUserByMail,
-  updateNewsletterConsent,
-} = require('../../../shared/users');
+/**
+ * This endpoint serves as a callback for when users unsubscribe via mailjet
+ */
+
+const { getUserByMail } = require('../../../shared/users');
 const { username, password } = require('../../../../basicAuth');
 const { errorResponse } = require('../../../shared/apiResponse');
+const AWS = require('aws-sdk');
+
+const config = { region: 'eu-central-1' };
+const ddb = new AWS.DynamoDB.DocumentClient(config);
+const tableName = process.env.USERS_TABLE_NAME;
 
 module.exports.handler = async event => {
   try {
@@ -27,7 +33,7 @@ module.exports.handler = async event => {
       if (result.Count === 0) {
         console.log('No user found with the passed email');
       } else {
-        await updateNewsletterConsent(result.Items[0].cognitoId, false);
+        await updateUser(result.Items[0]);
       }
 
       // updating user was successful, return appropriate json
@@ -61,4 +67,41 @@ const isAuthorized = event => {
   const plainCreds = new Buffer(encodedCreds, 'base64').toString().split(':');
 
   return plainCreds[0] === username && plainCreds[1] === password;
+};
+
+// Unsubscribe from every newsletter by setting newsletter consent to false,
+// as well as setting value in every item in customNewsletters to false
+const updateUser = ({ cognitoId, customNewsletters }) => {
+  const timestamp = new Date().toISOString();
+
+  // Loop through custom newsletters and set the values to false
+  if (typeof customNewsletters !== 'undefined') {
+    for (const newsletter of customNewsletters) {
+      newsletter.timestamp = timestamp;
+      newsletter.value = false;
+      newsletter.extraInfo = false;
+    }
+  }
+
+  const data = {
+    ':newsletterConsent': {
+      value: false,
+      timestamp,
+    },
+    ':customNewsletters': customNewsletters,
+    ':updatedAt': timestamp,
+  };
+
+  const updateExpression =
+    'set newsletterConsent = :newsletterConsent, customNewsletters = :customNewsletters, updatedAt = :updatedAt';
+
+  return ddb
+    .update({
+      TableName: tableName,
+      Key: { cognitoId },
+      UpdateExpression: updateExpression,
+      ExpressionAttributeValues: data,
+      ReturnValues: 'UPDATED_NEW',
+    })
+    .promise();
 };
