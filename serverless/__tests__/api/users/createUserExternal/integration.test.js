142b5b5cf3b29e92dd685e77389f3d03adc71592
Valioesi <Vali_Schagerl@web.de>
2020-12-01 15:17:17 +0100

    Updated tests

--- serverless/__tests__/api/users/createUserExternal/integration.test.js
+++ serverless/__tests__/api/users/createUserExternal/integration.test.js
@@ -7,6 +7,7 @@ const fetch = require('node-fetch');
 const randomWords = require('random-words');
 const crypto = require('crypto-secure-random-digit');
 const AWS = require('aws-sdk');
+const uuid = require('uuid/v4');
 
 const ddb = new AWS.DynamoDB.DocumentClient({ region: 'eu-central-1' });
 
@@ -15,22 +16,36 @@ const randomAgs = crypto.randomDigits(6).join('');
 const { token } = require('../../../../queryToken');
 const { getUser } = require('../../../../../utils/shared/users/getUsers');
 
+const AGS = '120312';
+
 describe('createUserFromExternal api test', () => {
-  it('should create a new user with existing municipality', async () => {
+  beforeAll(async () => {
+    await createMunicipality({ ags: AGS, name: 'Eisenhüttenstadt' });
+    await createMunicipality({ ags: randomAgs, name: 'Hobbingen' });
+  });
+
+  afterAll(async () => {
+    await deleteMunicipality(AGS);
+    await deleteMunicipality(randomAgs);
+  });
+
+  it('should create a new user', async () => {
     const randomEmail = `${randomWords()}.${randomWords()}@expedition-grundeinkommen.de`;
-    const ags = '120312';
+    const signupId = '23123';
+    const userToken = uuid();
+    const phoneNumber = '004915755940728';
 
     const request = {
       method: 'POST',
       mode: 'cors',
       body: JSON.stringify({
         email: randomEmail,
-        zipCode: '12051',
+        signupId,
         username: 'Vali',
-        city: 'Berlin',
-        firstName: 'Valentin',
-        lastName: 'Schagerl',
-        ags,
+        optedIn: true,
+        userToken,
+        ags: AGS,
+        phone: phoneNumber,
       }),
     };
 
@@ -46,44 +61,102 @@ describe('createUserFromExternal api test', () => {
 
     // Get user and municpality to check if saved correctly
     const { Item: user } = await getUser(DEV_USERS_TABLE, json.data.userId);
-    const { Item: municipality } = await getMunicipality(ags);
+    const { Item: municipality } = await getMunicipality(AGS);
 
     // Check user
     expect(user.cognitoId).toEqual(json.data.userId);
-    expect(user.firstName).toEqual('Valentin');
-    expect(user.lastName).toEqual('Schagerl');
+    expect(user.mgeSignupId).toEqual(signupId);
+    expect(user.confirmed.value).toEqual(true);
+    expect(user.confirmed.optedInAtMge).toEqual(true);
     expect(user.username).toEqual('Vali');
-    expect(user.zipCode).toEqual('12051');
-    expect(user.city).toEqual('Berlin');
+    expect(user.customToken.token).toEqual(userToken);
     expect(user.email).toEqual(randomEmail);
+    expect(user.phoneNumber).toEqual(phoneNumber);
+
     expect(user.municipalCampaigns.length).toEqual(1);
-    expect(user.municipalCampaigns[0].ags).toEqual(ags);
+    expect(user.municipalCampaigns[0].ags).toEqual(AGS);
+    expect(user.municipalCampaigns[0]).toHaveProperty('createdAt');
+
+    // Check municipality
+    const userInMunicipality = municipality.users[0];
+
+    expect(municipality.users.length).toEqual(1);
+    expect(municipality.ags).toEqual(AGS);
+    expect(userInMunicipality.id).toEqual(json.data.userId);
+    expect(userInMunicipality).toHaveProperty('createdAt');
+  });
+
+  it('should create a second user without phone', async () => {
+    const randomEmail = `${randomWords()}.${randomWords()}@expedition-grundeinkommen.de`;
+    const signupId = '23123';
+    const userToken = uuid();
+
+    const request = {
+      method: 'POST',
+      mode: 'cors',
+      body: JSON.stringify({
+        email: randomEmail,
+        signupId,
+        username: 'Vali',
+        optedIn: true,
+        userToken,
+        ags: AGS,
+      }),
+    };
+
+    const response = await fetch(
+      `${INVOKE_URL}/users/external-signup?token=${token}`,
+      request
+    );
+    const json = await response.json();
+
+    expect(response.status).toEqual(201);
+    expect(json).toHaveProperty('data');
+    expect(json.data).toHaveProperty('userId');
+
+    // Get user and municpality to check if saved correctly
+    const { Item: user } = await getUser(DEV_USERS_TABLE, json.data.userId);
+    const { Item: municipality } = await getMunicipality(AGS);
+
+    // Check user
+    expect(user.cognitoId).toEqual(json.data.userId);
+    expect(user.mgeSignupId).toEqual(signupId);
+    expect(user.confirmed.value).toEqual(true);
+    expect(user.confirmed.optedInAtMge).toEqual(true);
+    expect(user.username).toEqual('Vali');
+    expect(user.customToken.token).toEqual(userToken);
+    expect(user.email).toEqual(randomEmail);
+    expect(typeof user.phoneNumber).toEqual('undefined');
+
+    expect(user.municipalCampaigns.length).toEqual(1);
+    expect(user.municipalCampaigns[0].ags).toEqual(AGS);
     expect(user.municipalCampaigns[0]).toHaveProperty('createdAt');
 
     // Check municipality
     const userInMunicipality =
       municipality.users[municipality.users.length - 1];
 
-    expect(municipality.ags).toEqual(ags);
+    expect(municipality.users.length).toEqual(2);
+    expect(municipality.ags).toEqual(AGS);
     expect(userInMunicipality.id).toEqual(json.data.userId);
     expect(userInMunicipality).toHaveProperty('createdAt');
   });
 
-  it('should create a new user with new municipality', async () => {
+  it('should create a third user who is not optedIn', async () => {
     const randomEmail = `${randomWords()}.${randomWords()}@expedition-grundeinkommen.de`;
-    const ags = crypto.randomDigits(6).join('');
+    const signupId = '23123';
+    const userToken = uuid();
 
     const request = {
       method: 'POST',
       mode: 'cors',
       body: JSON.stringify({
         email: randomEmail,
-        zipCode: '12051',
+        signupId,
         username: 'Vali',
-        city: 'Berlin',
-        firstName: 'Valentin',
-        lastName: 'Schagerl',
-        ags,
+        optedIn: false,
+        userToken,
+        ags: AGS,
       }),
     };
 
@@ -99,38 +172,44 @@ describe('createUserFromExternal api test', () => {
 
     // Get user and municpality to check if saved correctly
     const { Item: user } = await getUser(DEV_USERS_TABLE, json.data.userId);
-    const { Item: municipality } = await getMunicipality(ags);
+    const { Item: municipality } = await getMunicipality(AGS);
 
     // Check user
     expect(user.cognitoId).toEqual(json.data.userId);
-    expect(user.firstName).toEqual('Valentin');
-    expect(user.lastName).toEqual('Schagerl');
+    expect(user.mgeSignupId).toEqual(signupId);
+    expect(user.confirmed.value).toEqual(false);
     expect(user.username).toEqual('Vali');
-    expect(user.zipCode).toEqual('12051');
-    expect(user.city).toEqual('Berlin');
+    expect(user.customToken.token).toEqual(userToken);
     expect(user.email).toEqual(randomEmail);
+    expect(typeof user.phoneNumber).toEqual('undefined');
+
     expect(user.municipalCampaigns.length).toEqual(1);
-    expect(user.municipalCampaigns[0].ags).toEqual(ags);
+    expect(user.municipalCampaigns[0].ags).toEqual(AGS);
     expect(user.municipalCampaigns[0]).toHaveProperty('createdAt');
 
     // Check municipality
-    expect(municipality.ags).toEqual(ags);
-    expect(municipality.users.length).toEqual(1);
-    expect(municipality.users[0].id).toEqual(json.data.userId);
-    expect(municipality.users[0]).toHaveProperty('createdAt');
+    const userInMunicipality =
+      municipality.users[municipality.users.length - 1];
+
+    expect(municipality.users.length).toEqual(3);
+    expect(municipality.ags).toEqual(AGS);
+    expect(userInMunicipality.id).toEqual(json.data.userId);
+    expect(userInMunicipality).toHaveProperty('createdAt');
   });
 
   it('should add municipality to existing user', async () => {
+    const signupId = '23123';
+    const userToken = uuid();
+
     const request = {
       method: 'POST',
       mode: 'cors',
       body: JSON.stringify({
         email,
-        zipCode: '12051',
+        signupId,
         username: 'Vali',
-        city: 'Berlin',
-        firstName: 'Valentin',
-        lastName: 'Schagerl',
+        optedIn: true,
+        userToken,
         ags: randomAgs,
       }),
     };
@@ -150,12 +229,11 @@ describe('createUserFromExternal api test', () => {
     const { Item: municipality } = await getMunicipality(randomAgs);
 
     // Check user
-    expect(user.cognitoId).toEqual(json.data.userId);
-    expect(user.firstName).toEqual('Valentin');
-    expect(user.lastName).toEqual('Schagerl');
+    expect(user.mgeSignupId).toEqual(signupId);
+    expect(user.confirmed.value).toEqual(true);
+    expect(typeof user.confirmed.optedInAtMge).toEqual('undefined');
     expect(user.username).toEqual('Vali');
-    expect(user.zipCode).toEqual('12051');
-    expect(user.city).toEqual('Berlin');
+    expect(user.customToken.token).toEqual(userToken);
     expect(user.email).toEqual(email);
 
     const municipalityInUser =
@@ -165,25 +243,27 @@ describe('createUserFromExternal api test', () => {
     expect(municipalityInUser).toHaveProperty('createdAt');
 
     // Check municipality
-    const userInMunicipality =
-      municipality.users[municipality.users.length - 1];
+    const userInMunicipality = municipality.users[0];
 
+    expect(municipality.users.length).toEqual(1);
     expect(municipality.ags).toEqual(randomAgs);
     expect(userInMunicipality.id).toEqual(json.data.userId);
     expect(userInMunicipality).toHaveProperty('createdAt');
   });
 
-  it('should not add existing municipality to existing user', async () => {
+  it('should not add municipality to existing user', async () => {
+    const signupId = '23123';
+    const userToken = uuid();
+
     const request = {
       method: 'POST',
       mode: 'cors',
       body: JSON.stringify({
         email,
-        zipCode: '12051',
+        signupId,
         username: 'Vali',
-        city: 'Berlin',
-        firstName: 'Valentin',
-        lastName: 'Schagerl',
+        optedIn: true,
+        userToken,
         ags: randomAgs,
       }),
     };
@@ -200,16 +280,18 @@ describe('createUserFromExternal api test', () => {
   });
 
   it('should have missing query token', async () => {
+    const signupId = '23123';
+    const userToken = uuid();
+
     const request = {
       method: 'POST',
       mode: 'cors',
       body: JSON.stringify({
         email,
-        zipCode: '12051',
+        signupId,
         username: 'Vali',
-        city: 'Berlin',
-        firstName: 'Valentin',
-        lastName: 'Schagerl',
+        optedIn: true,
+        userToken,
         ags: randomAgs,
       }),
     };
@@ -223,16 +305,18 @@ describe('createUserFromExternal api test', () => {
   });
 
   it('should have wrong query token', async () => {
+    const signupId = '23123';
+    const userToken = uuid();
+
     const request = {
       method: 'POST',
       mode: 'cors',
       body: JSON.stringify({
         email,
-        zipCode: '12051',
+        signupId,
         username: 'Vali',
-        city: 'Berlin',
-        firstName: 'Valentin',
-        lastName: 'Schagerl',
+        optedIn: true,
+        userToken,
         ags: randomAgs,
       }),
     };
@@ -246,15 +330,17 @@ describe('createUserFromExternal api test', () => {
   });
 
   it('should have missing email', async () => {
+    const signupId = '23123';
+    const userToken = uuid();
+
     const request = {
       method: 'POST',
       mode: 'cors',
       body: JSON.stringify({
-        zipCode: '12051',
+        signupId,
         username: 'Vali',
-        city: 'Berlin',
-        firstName: 'Valentin',
-        lastName: 'Schagerl',
+        optedIn: true,
+        userToken,
         ags: randomAgs,
       }),
     };
@@ -277,4 +363,24 @@ const getMunicipality = ags => {
   };
 
   return ddb.get(params).promise();
+};
+
+const createMunicipality = municipality => {
+  const params = {
+    TableName: DEV_MUNICIPALITIES_TABLE,
+    Item: municipality,
+  };
+
+  return ddb.put(params).promise();
+};
+
+const deleteMunicipality = ags => {
+  const params = {
+    TableName: DEV_MUNICIPALITIES_TABLE,
+    Key: {
+      ags,
+    },
+  };
+
+  return ddb.delete(params).promise();
 };
