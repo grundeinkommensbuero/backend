3f824c6ff0d0f2adb8c1af14c5f3a1be4ed5db58
Valioesi <Vali_Schagerl@web.de>
2020-12-01 14:22:24 +0100

    Sign up for munic. takes new request data

--- serverless/src/shared/utils/index.js
+++ serverless/src/shared/utils/index.js
@@ -39,10 +39,39 @@ const getRandomString = length => {
   return randomBytes(length).toString('hex');
 };
 
+// Use the same validation as in frontend
+const validateEmail = email => {
+  // eslint-disable-next-line no-useless-escape
+  const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regex.test(email.toLowerCase());
+};
+
+const validateZipCode = zipCode => {
+  const regex = /\b\d{5}\b/g;
+  return regex.test(zipCode.toString());
+};
+
+const validatePhoneNumber = phoneNumber => {
+  const regex = /^[0-9]*$/g;
+  return regex.test(phoneNumber);
+};
+
+// Strips input off any unwanted characters
+const formatPhoneNumber = phoneNumber => {
+  const string = phoneNumber.toString();
+
+  // Replace (,),-,/ and space with emtpy string and replace + with 00
+  return string.replace(new RegExp('[()/-\\s]', 'g'), '').replace('+', '00');
+};
+
 module.exports = {
   constructCampaignId,
   generateRandomId,
   getFileSuffix,
   formatNumber,
   getRandomString,
+  validateEmail,
+  validateZipCode,
+  validatePhoneNumber,
+  formatPhoneNumber,
 };
