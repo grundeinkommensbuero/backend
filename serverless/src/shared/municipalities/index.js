ed55c297a6c1984c7e481799c1c638b89a2ec34d
Valioesi <Vali_Schagerl@web.de>
2020-12-02 13:05:22 +0100

    Rewrote getPlacesStats to compute more detailed stats

--- serverless/src/shared/municipalities/index.js
+++ serverless/src/shared/municipalities/index.js
@@ -14,7 +14,7 @@ const getMunicipality = ags => {
   return ddb.get(params).promise();
 };
 
-const getAllMunicipalites = async (municipalities = [], startKey = null) => {
+const getAllMunicipalities = async (municipalities = [], startKey = null) => {
   const params = {
     TableName: municipalitiesTableName,
   };
@@ -30,7 +30,37 @@ const getAllMunicipalites = async (municipalities = [], startKey = null) => {
   // call same function again, if the whole table has not been scanned yet
   if ('LastEvaluatedKey' in result) {
     console.log('call get lists recursively');
-    return getAllMunicipalites(municipalities, result.LastEvaluatedKey);
+    return getAllMunicipalities(municipalities, result.LastEvaluatedKey);
+  }
+
+  // otherwise return the array
+  return municipalities;
+};
+
+const getAllMunicipalitiesWithUsers = async (
+  municipalities = [],
+  startKey = null
+) => {
+  const params = {
+    TableName: municipalitiesTableName,
+    FilterExpression: 'attribute_exists(users)',
+  };
+  if (startKey !== null) {
+    params.ExclusiveStartKey = startKey;
+  }
+
+  const result = await ddb.scan(params).promise();
+
+  // add elements to existing array
+  municipalities.push(...result.Items);
+
+  // call same function again, if the whole table has not been scanned yet
+  if ('LastEvaluatedKey' in result) {
+    console.log('call get lists recursively');
+    return getAllMunicipalitiesWithUsers(
+      municipalities,
+      result.LastEvaluatedKey
+    );
   }
 
   // otherwise return the array
@@ -39,5 +69,6 @@ const getAllMunicipalites = async (municipalities = [], startKey = null) => {
 
 module.exports = {
   getMunicipality,
-  getAllMunicipalites,
+  getAllMunicipalities,
+  getAllMunicipalitiesWithUsers,
 };
