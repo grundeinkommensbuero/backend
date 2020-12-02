38c8fd17631b28e9e95ce50688e9d7e8152ccec5
Valioesi <Vali_Schagerl@web.de>
2020-12-02 17:25:27 +0100

    Added script to fill municipalities table

--- utils/config.js
+++ utils/config.js
@@ -4,5 +4,7 @@ module.exports = {
   PROD_USER_POOL_ID: 'eu-central-1_xx4VmPPdF',
   DEV_USERS_TABLE_NAME: 'dev-users',
   DEV_SIGNATURES_TABLE_NAME: 'dev-signatures',
+  DEV_MUNICIPALITIES_TABLE_NAME: 'dev-municipalities',
+  PROD_MUNICIPALITIES_TABLE_NAME: 'prod-municipalities',
   DEV_USER_POOL_ID: 'eu-central-1_SYtDaO0qH',
 };
