06c97a1bc88017743dd0d1f56e0ecddd0a16b156
Valioesi <Vali_Schagerl@web.de>
2020-12-10 10:22:52 +0100

    Refactored donation mail

--- serverless/__tests__/api/users/updateUser/unit.test.js
+++ serverless/__tests__/api/users/updateUser/unit.test.js
@@ -6,9 +6,19 @@ describe('Test computeDebitDate', () => {
 
     const debitDate = computeDebitDate(date);
 
-    expect(debitDate.getDate()).toEqual(16);
+    expect(debitDate.getDate()).toEqual(9);
     expect(debitDate.getMonth()).toEqual(11);
     expect(debitDate.getFullYear()).toEqual(2020);
+  });
+
+  it('should compute correct date', () => {
+    const date = new Date('2020-12-09');
+
+    const debitDate = computeDebitDate(date);
+
+    expect(debitDate.getDate()).toEqual(13);
+    expect(debitDate.getMonth()).toEqual(0);
+    expect(debitDate.getFullYear()).toEqual(2021);
   });
 
   it('should compute correct date', () => {
--- serverless/src/api/users/updateUser/sendMail.js
+++ serverless/src/api/users/updateUser/sendMail.js
@@ -1,12 +1,22 @@
 const { apiKey, apiSecret } = require('../../../../mailjetConfig');
 const mailjet = require('node-mailjet').connect(apiKey, apiSecret);
+// IDEAL BOLD 19
 
 // Function which sends an email to the user after donation was changed
 const sendMail = (
   email,
-  { recurring, amount, firstName, lastName, iban },
-  recurringDonationExisted
+  { recurring, amount, firstName, lastName },
+  { debitDate, id, recurringDonationExisted }
 ) => {
+  let amountAsString = amount.toString().replace('.', ',');
+  if (amountAsString.includes(',')) {
+    if (amountAsString.split(',')[1].length === 1) {
+      amountAsString = `${amountAsString}0`;
+    }
+  } else {
+    amountAsString = `${amountAsString},00`;
+  }
+
   return mailjet.post('send', { version: 'v3.1' }).request({
     Messages: [
       {
@@ -25,10 +35,11 @@ const sendMail = (
         Variables: {
           recurring,
           recurringDonationExisted,
-          amount,
+          amount: amountAsString,
           firstName,
           lastName,
-          iban,
+          debitDate: formatDate(debitDate),
+          id,
         },
       },
     ],
@@ -36,3 +47,7 @@ const sendMail = (
 };
 
 module.exports = sendMail;
+
+const formatDate = date => {
+  return `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
+};
