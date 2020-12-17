c95084858610f7a5e6544b9e682d84f563f44dcb
Valentin Schagerl <Vali_Schagerl@web.de>
2020-12-17 15:08:33 +0100

    Merged #66: Municipalities stats, login via token & external signup (deactivated)
    * New endpoint to sign up user for municipality
    
    * Check for token in db when signing in
    
    * Sign up for munic. takes new request data
    
    * Save phone number and user token
    
    * Forgot to save signupId when updating user
    
    * Updated tests
    
    * Possibility to remove token in updateUser
    
    * New endpoint to sign up user for municipality
    
    * New endpoint to get stats for municipalities
    
    * Rewrote getPlacesStats to compute more detailed stats
    
    * Added script to fill municipalities table
    
    * Updated tests and fixed bug
    
    * Strip token from user in getCurrentUser
    
    * Refactored places endpoints into municaplities endpoints and updated tests + docs
    
    * Updated tests, docs and fixed bug
    
    * Updated gitignore
    
    * Added file to fill municipalities
    
    * add local  mockup script
    
    * gitignore exception: meinbge user data
    
    * Script to fill municipalities table
    
    * Added query token and fixed config
    
    * Updated request body for createUserExternal
    
    * Updated tests and fixed bug in createUserExternal
    
    * Updated docs for /external-signup
    
    * Updated script to fill municipality table
    
    * getMunicipalityStats: new db schema + filters
    
    * Added env variable in yml for new table
    
    * Typo in seeding script
    
    * Rewrote /analytics/municipalities with saving stats as json file
    
    * Add GSI to userMunicipalityTable
    
    * Rewrote getStats for one munic. for new db structure
    
    * Fixed issue and updated test for getting stats for one munic
    
    * Fixed bugs
    
    * Bugfix in createStats and updated test
    
    * Updated docs
    
    * Removed timing logs
    
    * fix slice error, update thresholds and logic for wins and newcomers
    
    * Rewrote createUserExternal due to new db structure
    
    * Added more validation + optional params can be null
    
    * Fixed comment regarding expiration time of code
    
    * Comment out createUserExternal in serverless config
    
    * Ignore createUserExternal test
    
    Co-authored-by: felixbuchholz <felixbuchholz@gmx.net>

---
+++ utils/seedDatabase/fillMunicipalities/createMockData.js
@@ -0,0 +1,231 @@
+const fs = require('fs');
+const csv = require('csv-parser');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const d3 = require('d3-scale');
+
+// Params
+
+const scalePromilleToGoal = 'scaleLinear';
+const rangePromilleToGoal = [0, 400];
+const plz3Variance = [1, 4];
+
+// ---- Utils ----------------------------------------------------------------------------
+
+const readCSV = path => {
+  return new Promise((res, rej) => {
+    const arr = [];
+    fs.createReadStream(path)
+      .pipe(csv())
+      .on('data', data => arr.push(data))
+      .on('end', () => {
+        res(arr);
+      });
+  });
+};
+
+const getRandom = (min, max) => {
+  const random = Math.random() * (max - min) + min;
+  // return +random.toFixed(2);
+  return random;
+};
+
+//  **************************************************************************************
+
+// Paths to merge
+const plzUsersPath = './data/plz-3-meinbge-user-promille.csv';
+const flatDataPath = './data/flatData.json';
+// Output
+const outputPath = './output/mockup-complete.json';
+const gemeindenOutput = './output/municipalities.json';
+const signupsOutput = './output/signups.json';
+const responseOutput = './output/response.json';
+
+const readFiles = async () => {
+  const plzUsers = await readCSV(plzUsersPath);
+  const flatDataRaw = fs.readFileSync(flatDataPath, 'utf8');
+  const flatData = JSON.parse(flatDataRaw);
+  return { plzUsers, flatData };
+};
+
+const mockupData = async () => {
+  let { plzUsers, flatData, goals } = await readFiles();
+  // Check data structure:
+  // console.log(plzUsers, flatData);
+  // console.log(plzUsers[0], flatData[0]);
+
+  // Each ags should have:
+  // goal
+  // percentToGoal
+  // --> signups
+
+  const roundTo = (number, factor) => {
+    return Math.round(number / factor) * factor;
+  };
+
+  const prettifyNumber = number => {
+    let pretty = number;
+    let steps = [
+      { threshold: 20, roundTo: 1 },
+      { threshold: 50, roundTo: 5 },
+      { threshold: 150, roundTo: 10 },
+      { threshold: 400, roundTo: 50 },
+      { threshold: 4000, roundTo: 100 },
+      { threshold: 10000, roundTo: 500 },
+      { threshold: 40000, roundTo: 1000 },
+      { threshold: 100000, roundTo: 5000 },
+      { threshold: Infinity, roundTo: 10000 },
+    ];
+    const step = steps.find(x => number < x.threshold);
+    pretty = roundTo(number, step.roundTo);
+    return pretty;
+  };
+
+  const getGoal = (population, minGoal = 7, goalFactor = 0.01) => {
+    let goal = population * goalFactor;
+    goal = Math.max(minGoal, prettifyNumber(goal));
+    return goal;
+  };
+
+  // Goal
+  flatData = flatData.map((x, i) => {
+    const goal = getGoal(x.population);
+    return { ...x, goal };
+  });
+
+  // Percent to goal
+  // Match plzUsers to data
+  // Check extent of promille and setup a scale
+  const userPromille = plzUsers.map(x => x.promille);
+  const minPromille = Math.min(...userPromille);
+  const maxPromille = Math.max(...userPromille);
+  const promilleToGoalScale = d3[scalePromilleToGoal]()
+    .domain([0, maxPromille + plz3Variance[1] / 2])
+    .range(rangePromilleToGoal);
+  console.log('Promille extent: ', minPromille, maxPromille);
+
+  // Add percentToGoal
+  flatData = flatData.map((x, i) => {
+    const promille = +plzUsers.find(y => y.plz === x.zipCode.substring(0, 3))
+      .promille;
+
+    const promilleVariance = getRandom(...plz3Variance);
+    const promilleMin = promille - promilleVariance;
+    const promilleMax = promille + promilleVariance;
+    let promillePlz = getRandom(promilleMin, promilleMax);
+    promillePlz = Math.max(0, promillePlz);
+    let percentToGoal = promilleToGoalScale(promillePlz);
+    percentToGoal = +percentToGoal.toFixed(3);
+    return { ...x, percentToGoal };
+  });
+  console.log('Added percentToGoal: ', flatData[0]);
+
+  const checkPercentToGoal = flatData.map(x => x.percentToGoal);
+  console.log(Math.min(...checkPercentToGoal), Math.max(...checkPercentToGoal));
+
+  const grouped = flatData.reduce((acc, cur, i, src) => {
+    const foundIndex = acc.findIndex(x => x.ags === cur.ags);
+    if (foundIndex === -1) {
+      const {
+        longitude,
+        latitude,
+        ags,
+        name,
+        district,
+        state,
+        population,
+        goal,
+      } = cur;
+      const zipCodes = [cur.zipCode];
+      const percentages = [cur.percentToGoal];
+      acc.push({
+        longitude,
+        latitude,
+        ags,
+        name,
+        zipCodes,
+        district,
+        state,
+        population,
+        goal,
+        percentages,
+      });
+    } else {
+      acc[foundIndex].zipCodes.push(cur.zipCode);
+      acc[foundIndex].percentages.push(cur.percentToGoal);
+    }
+
+    return acc;
+  }, []);
+  console.log(grouped[0]);
+
+  const groupedSignups = grouped.map(x => {
+    const { percentages, goal, ...rest } = x;
+    const percentTest =
+      percentages.reduce((a, b) => a + b) / percentages.length;
+
+    const signups = parseInt((goal * percentTest) / 100);
+    let percentToGoal = (signups / goal) * 100;
+    percentToGoal = +percentToGoal.toFixed(2);
+    return { ...rest, goal, signups, percentToGoal };
+  });
+  console.log(groupedSignups[0]);
+  fs.writeFileSync(outputPath, JSON.stringify(groupedSignups));
+
+  const gemeinden = groupedSignups.map(x => {
+    const { ags, name, longitude, latitude, population, goal } = x;
+    const coordinates = [longitude, latitude];
+    return { ags, name, coordinates, goal };
+  });
+  console.log(gemeinden[0]);
+  fs.writeFileSync(gemeindenOutput, JSON.stringify(gemeinden));
+
+  const gemeindenCSV = groupedSignups.map(x => {
+    const { ags, name, longitude, latitude, population } = x;
+    return { ags, name, longitude, latitude, population };
+  });
+  const csvWriter = createCsvWriter({
+    path: './output/gemeinden.csv',
+    header: [
+      { id: 'ags', title: 'ags' },
+      { id: 'name', title: 'name' },
+      { id: 'longitude', title: 'longitude' },
+      { id: 'latitude', title: 'latitude' },
+      { id: 'population', title: 'population' },
+    ],
+  });
+  csvWriter.writeRecords(gemeindenCSV);
+
+  const signups = groupedSignups.map(x => {
+    const { ags, signups } = x;
+    return { ags, signups };
+  });
+  console.log(signups[0]);
+  fs.writeFileSync(signupsOutput, JSON.stringify(signups));
+
+  const municipalities = groupedSignups.map(x => {
+    const { ags, signups } = x;
+    return { ags, signups };
+  });
+
+  console.log(municipalitises[0]);
+
+  const response = {
+    events: [{ ags: '11000000', signups: [30000, 40000] }],
+    municipalities,
+    scale: [
+      [1, 40000],
+      [2000, 80000],
+    ],
+    timePassed: 6,
+  };
+  console.log(response);
+  fs.writeFileSync(responseOutput, JSON.stringify(response));
+
+  const characterSet = [
+    ...new Set(gemeinden.map(x => x.name.split('')).flat()),
+  ];
+  console.log(characterSet, characterSet.length);
+  fs.writeFileSync('./output/characterSet.json', JSON.stringify(characterSet));
+};
+
+mockupData();
---
+++ utils/seedDatabase/fillMunicipalities/fillDatabase.js
@@ -0,0 +1,59 @@
+const signups = require('./output/mockup-complete.json');
+const { DEV_USER_MUNICIPALITY_TABLE_NAME } = require('../../config');
+const AWS = require('aws-sdk');
+const Bottleneck = require('bottleneck');
+const uuid = require('uuid/v4');
+const fs = require('fs');
+
+const config = { region: 'eu-central-1' };
+const ddb = new AWS.DynamoDB.DocumentClient(config);
+
+const limiter = new Bottleneck({ minTime: 100, maxConcurrent: 4 });
+
+const fillDatabase = async () => {
+  for (const signup of signups) {
+    await limiter.schedule(async () => {
+      for (let i = 0; i < signup.signups; i++) {
+        await updateUserMunicipalityTable(
+          signup.ags,
+          uuid(),
+          createRandomDate().toISOString(),
+          signup.population
+        );
+      }
+
+      console.log('Updated', signup.ags);
+    });
+  }
+};
+
+const updateUserMunicipalityTable = (ags, userId, createdAt, population) => {
+  const params = {
+    TableName: DEV_USER_MUNICIPALITY_TABLE_NAME,
+    Item: {
+      ags,
+      userId,
+      createdAt,
+      population,
+    },
+  };
+
+  return ddb.put(params).promise();
+};
+
+const createRandomDate = () => {
+  const date = new Date();
+  const pastDate = new Date();
+
+  pastDate.setDate(date.getDate() - 4);
+
+  const randomDate = new Date(
+    pastDate.getTime() +
+      Math.floor(Math.random() * (date.getTime() - pastDate.getTime())) +
+      1
+  );
+
+  return randomDate;
+};
+
+fillDatabase();
