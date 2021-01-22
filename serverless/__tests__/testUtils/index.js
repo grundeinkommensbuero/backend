0dd4480ad3affdc302ada577e02ba563fc3c86cb
Valioesi <Vali_Schagerl@web.de>
2021-01-22 12:09:58 +0100

    Fix getToken script

--- serverless/__tests__/testUtils/index.js
+++ serverless/__tests__/testUtils/index.js
@@ -17,8 +17,8 @@ const ddb = new AWS.DynamoDB.DocumentClient({ region: 'eu-central-1' });
 const config = { region: 'eu-central-1' };
 const cognito = new AWS.CognitoIdentityServiceProvider(config);
 
-module.exports.authenticate = async () => {
-  return makeAuthenticationCall(USER_POOL_ID, CLIENT_ID, USER_ID);
+module.exports.authenticate = async (userId = USER_ID) => {
+  return makeAuthenticationCall(USER_POOL_ID, CLIENT_ID, userId);
 };
 
 module.exports.authenticateAdmin = async () => {
