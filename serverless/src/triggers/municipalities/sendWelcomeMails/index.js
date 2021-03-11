468698908e66617e08994ea9ce125933f7fea450
Valioesi <Vali_Schagerl@web.de>
2021-03-11 15:56:00 +0100

    Updated welcome mails

--- serverless/src/triggers/municipalities/sendWelcomeMails/index.js
+++ serverless/src/triggers/municipalities/sendWelcomeMails/index.js
@@ -3,18 +3,13 @@
  * for a municipality. Those users will receive an email.
  */
 
-const AWS = require('aws-sdk');
 const { sendErrorMail } = require('../../../shared/errorHandling');
 const {
   getAllMunicipalitiesWithUsers,
+  getMunicipality,
 } = require('../../../shared/municipalities');
 const { getUser } = require('../../../shared/users');
 
-const s3 = new AWS.S3();
-
-const bucket = 'xbge-municipalities-stats';
-const stage = process.env.STAGE;
-const fileName = 'statsWithAll.json';
 const sendMail = require('./sendMail');
 
 const ONE_DAY = 24 * 60 * 60 * 1000;
@@ -26,14 +21,8 @@ module.exports.handler = async event => {
       new Date(new Date() - ONE_DAY).toISOString()
     );
 
-    // We need the stats to check if a municipality has reached goal.
-    // We don't want to compute the stats again, because we already
-    // do it in a cron job, so we just get the json from s3
-    const json = await getJson();
-    const { municipalities } = JSON.parse(json.Body.toString());
-
     // Send mails to all those users
-    await sendMails(userMunicipalityLinks, municipalities);
+    await sendMails(userMunicipalityLinks);
   } catch (error) {
     console.log('Error sending welcome mails', error);
     await sendErrorMail('sendWelcomeMails', error);
@@ -43,30 +32,28 @@ module.exports.handler = async event => {
 };
 
 // Send mails to all user who have signed up for municipality
-const sendMails = async (userMunicipalityLinks, municipalities) => {
-  for (const { userId, ags, wantsToBeActive } of userMunicipalityLinks) {
+const sendMails = async userMunicipalityLinks => {
+  for (const { userId, ags } of userMunicipalityLinks) {
     // Get user record from users table to get email, username etc
-    const result = await getUser(userId);
+    const userResult = await getUser(userId);
 
-    if ('Item' in result) {
-      // Get stats for this municipality
-      const found = municipalities.find(
-        municipality => municipality.ags === ags
-      );
+    // Get municipality record to get municipality name
+    const municipalityResult = await getMunicipality(ags);
 
-      const reachedGoal = found && found.signups >= found.goal;
-
-      await sendMail(result.Item, reachedGoal, wantsToBeActive);
+    // Only send the email if user still exists and is a new user (created after launch)
+    // and has newsletter consent
+    if (
+      'Item' in userResult &&
+      userWasCreatedAfterLaunch(userResult.Item) &&
+      userResult.Item.newsletterConsent.value &&
+      'Item' in municipalityResult
+    ) {
+      await sendMail(userResult.Item, municipalityResult.Item);
+      console.log('sent mail to', userResult.Item.email);
     }
   }
 };
 
-// Gets json file from s3
-const getJson = () => {
-  const params = {
-    Bucket: bucket,
-    Key: `${stage}/${fileName}`,
-  };
-
-  return s3.getObject(params).promise();
+const userWasCreatedAfterLaunch = user => {
+  return new Date(user.createdAt) > new Date('2021-02-22');
 };
