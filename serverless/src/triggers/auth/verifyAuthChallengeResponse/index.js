76fd0bd62f3873c69061873b453628f061b91829
Valentin Schagerl <Vali_Schagerl@web.de>
2021-05-12 10:32:39 +0200

    Revert "Rewrote auth using /sign-in endpoint (#107)" (#110)
    This reverts commit 1ada4ded4c882ca9d5eb9cc6fdb231a391a164d2.

--- serverless/src/triggers/auth/verifyAuthChallengeResponse/index.js
+++ serverless/src/triggers/auth/verifyAuthChallengeResponse/index.js
@@ -1,51 +1,16 @@
-const { apiKey, apiSecret } = require('../../../../mailjetConfig');
-const mailjet = require('node-mailjet').connect(apiKey, apiSecret);
-
-const LINK_TIMEOUT_SECONDS = 20 * 60; // number of seconds the magic link should be valid
+const { sendEmailViaSes } = require('../createAuthChallenge');
 
 exports.handler = async event => {
-  // Get challenge and timestamp from user attributes
-  const [authChallenge, timestamp] = (
-    event.request.privateChallengeParameters.challenge || ''
-  ).split(',');
-
-  // Check if code is equal to what we expect and whether the link hasn't timed out...
-  if (
-    event.request.challengeAnswer === authChallenge &&
-    Number(timestamp) > new Date().valueOf() / 1000 - LINK_TIMEOUT_SECONDS
-  ) {
+  const expectedAnswer =
+    event.request.privateChallengeParameters.secretLoginCode;
+  if (event.request.challengeAnswer === expectedAnswer) {
     event.response.answerCorrect = true;
   } else if (event.request.challengeAnswer === 'resendCode') {
-    // We want to send the second mail via Mailjet as a fallback
-    await sendEmail(event.request.userAttributes.email, authChallenge);
+    // We want to send the second mail via SES as a fallback
+    await sendEmailViaSes(event.request.userAttributes.email, expectedAnswer);
     event.response.answerCorrect = false;
   } else {
     event.response.answerCorrect = false;
   }
-
   return event;
-};
-
-const sendEmail = (userAttributes, code) => {
-  return mailjet.post('send', { version: 'v3.1' }).request({
-    Messages: [
-      {
-        To: [
-          {
-            Email: userAttributes.email,
-          },
-        ],
-        TemplateID: 1583518,
-        TemplateLanguage: true,
-        TemplateErrorReporting: {
-          Email: 'valentin@expedition-grundeinkommen.de',
-          Name: 'Vali',
-        },
-        // TemplateErrorDeliver: true,
-        Variables: {
-          code,
-        },
-      },
-    ],
-  });
 };
