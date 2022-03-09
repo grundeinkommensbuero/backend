80b194928610ef5b9bbc2d6ff7c9bce0688bc140
Valioesi <Vali_Schagerl@web.de>
2022-03-09 17:24:10 +0100

    Finalized templates for A flow

--- serverless/src/triggers/sendReminderMails/sendMail.js
+++ serverless/src/triggers/sendReminderMails/sendMail.js
@@ -4,20 +4,28 @@ const mailjet = require('node-mailjet').connect(apiKey, apiSecret);
 
 // TODO as soon as tempaltes are ready
 const mailTypeToTemplate = {
-  'B2.1': 1111,
-  'B2.2': 1111,
-  'B3.1': 1111,
-  'B3.2': 1111,
-  'B3.3': 1111,
-  'B4.1': 1111,
-  'B4.2': 1111,
-  'B4.3': 1111,
-  'B6.1': 1111,
-  'B6.2': 1111,
-  'B6.3': 1111,
+  'B2.1': 3719962,
+  'B2.2': 3719962,
+  'B3.1': 3720184,
+  'B3.2': 3720184,
+  'B3.3': 3720184,
+  'B4.1': 3720202,
+  'B4.2': 3720202,
+  'B4.3': 3720202,
+  'B6.1': 3717141,
+  'B6.2': 3717141,
+  'B6.3': 3717141,
 };
 
-// TODO: change params as soon as it is clear, what params the emails need
+const GOALS = {
+  'schleswig-holstein-1': '24.000',
+  'brandenburg-1': '24.000',
+  'hamburg-1': '12.000',
+  'bremen-1': '5.000',
+  'berlin-1': '24.000',
+  'berlin-2': '24.000',
+  'democracy-1': '20.000',
+};
 
 // Function which sends an email to remind user to send signature lists
 // gets a user object, which is why we destructure the object
@@ -25,12 +33,10 @@ const sendMail = (
   { email, username, cognitoId: userId },
   listId,
   campaign,
-  daysAgoListWasDownloaded,
+  daysSinceDownload,
   mailType,
   signatureCounts,
-  listCounts,
-  registeredSignatures,
-  attachments
+  listCounts
 ) => {
   return mailjet.post('send', { version: 'v3.1' }).request({
     Messages: [
@@ -41,7 +47,6 @@ const sendMail = (
           },
         ],
         TemplateID: mailTypeToTemplate[mailType],
-        Subject: 'TODO',
         TemplateLanguage: true,
         TemplateErrorReporting: {
           Email: 'valentin@expedition-grundeinkommen.de',
@@ -53,9 +58,21 @@ const sendMail = (
           username,
           userId,
           listId,
-          registeredSignatures,
+          campaign:
+            campaign.state === 'berlin'
+              ? 'Berlin soll Grundeinkommen testen'
+              : 'Demokratie für alle',
+          days: daysSinceDownload,
+          signatureCount:
+            campaign.code in signatureCounts
+              ? formatNumber(signatureCounts[campaign.code].computed)
+              : '0',
+          listCount:
+            campaign.code in listCounts
+              ? formatNumber(listCounts[campaign.code].total.downloads)
+              : '0',
+          goal: GOALS[campaign.code],
         },
-        Attachments: attachments,
       },
     ],
   });
