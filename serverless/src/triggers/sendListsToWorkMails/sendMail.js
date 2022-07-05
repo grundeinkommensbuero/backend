dfe06ae6c0fc199b46e9f22b65bac8eca2e4cba3
Valioesi <vali_schagerl@web.de>
2022-07-05 17:29:06 +0200

    Finalized lists to work mails

--- serverless/src/triggers/sendListsToWorkMails/sendMail.js
+++ serverless/src/triggers/sendListsToWorkMails/sendMail.js
@@ -2,14 +2,16 @@ const { apiKey, apiSecret } = require('../../../mailjetConfig');
 const mailjet = require('node-mailjet').connect(apiKey, apiSecret);
 
 const mailTypeToTemplate = {
-  1: 111,
-  2: 111,
-  3: 111,
+  1: 4039822,
+  2: 4044281,
+  3: 4044305,
+  4: 4044354,
+  5: 4044379,
 };
 
 // Function which sends an email to a user who has signed up for
 // take your lists to work campaign
-const sendMail = ({ email, username, userId }, mailType) => {
+const sendMail = ({ email, username, cognitoId: userId }, mailType) => {
   const templateID = mailTypeToTemplate[mailType];
 
   return mailjet.post('send', { version: 'v3.1' }).request({
