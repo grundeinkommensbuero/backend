757eff14093369d2b360d97bee8365897c891548
Valentin Schagerl <Vali_Schagerl@web.de>
2022-07-06 12:30:41 +0200

    Take lists to work (#157)
    * Draft for automatic mail for lists to work campaign
    
    * Finalized lists to work mails

--- serverless/src/shared/utils/index.js
+++ serverless/src/shared/utils/index.js
@@ -71,7 +71,8 @@ const getMunicipalityGoal = (population, minGoal = 7, goalFactor = 0.01) => {
 // Use the same validation as in frontend
 const validateEmail = email => {
   // eslint-disable-next-line no-useless-escape
-  const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const regex =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return regex.test(email.toLowerCase());
 };
 
@@ -187,4 +188,5 @@ module.exports = {
   validateWantsToCollect,
   sleep,
   isXDaysAgo,
+  isToday,
 };
