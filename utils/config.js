5165ff745ccbfec87926662a4d7a2fa2ebed85b2
Valioesi <vali_schagerl@web.de>
2022-08-23 19:03:46 +0200

    Script to analyse vouchers

--- utils/config.js
+++ utils/config.js
@@ -3,6 +3,7 @@ const SECRET_CONFIG = require('./secretConfig');
 module.exports = {
   PROD_USERS_TABLE_NAME: 'prod-users',
   PROD_SIGNATURES_TABLE_NAME: 'prod-signatures',
+  PROD_VOUCHERS_TABLE_NAME: 'prod-vouchers',
   PROD_USER_POOL_ID: 'eu-central-1_xx4VmPPdF',
   DEV_USERS_TABLE_NAME: 'dev-users',
   DEV_SIGNATURES_TABLE_NAME: 'dev-signatures',
